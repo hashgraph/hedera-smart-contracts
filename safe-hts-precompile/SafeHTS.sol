922aa0d043aa5f37d08d3d6c08a843e066cffacf
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2022-08-18 14:38:44 +0300

    Use IHederaTokenService instead of IHTS
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- safe-hts-precompile/SafeHTS.sol
+++ safe-hts-precompile/SafeHTS.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.5.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import "./IHTS.sol";
+import "../hts-precompile/IHederaTokenService.sol";
 import "./HederaResponseCodes.sol";
 
 library SafeHTS {
@@ -11,19 +11,19 @@ library SafeHTS {
     // 90 days in seconds
     uint32 constant defaultAutoRenewPeriod = 7776000;
 
-    function safeCryptoTransfer(IHTS.TokenTransferList[] memory tokenTransfers) internal {
+    function safeCryptoTransfer(IHederaTokenService.TokenTransferList[] memory tokenTransfers) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.cryptoTransfer.selector, tokenTransfers));
+            abi.encodeWithSelector(IHederaTokenService.cryptoTransfer.selector, tokenTransfers));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe crypto transfer failed!");
     }
 
-    function safeMintToken(IHTS token, uint64 amount, bytes[] memory metadata) internal
+    function safeMintToken(IHederaTokenService token, uint64 amount, bytes[] memory metadata) internal
     returns (uint64 newTotalSupply, int64[] memory serialNumbers) {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.delegatecall(
-            abi.encodeWithSelector(IHTS.mintToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.mintToken.selector,
             token, amount, metadata));
         (responseCode, newTotalSupply, serialNumbers) =
         success
@@ -32,12 +32,12 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe mint failed!");
     }
 
-    function safeBurnToken(IHTS token, uint64 amount, int64[] memory serialNumbers) internal
+    function safeBurnToken(IHederaTokenService token, uint64 amount, int64[] memory serialNumbers) internal
     returns (uint64 newTotalSupply)
     {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.burnToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.burnToken.selector,
             token, amount, serialNumbers));
         (responseCode, newTotalSupply) =
         success
@@ -49,16 +49,16 @@ library SafeHTS {
     function safeAssociateTokens(address account, address[] memory tokens) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.associateTokens.selector,
+            abi.encodeWithSelector(IHederaTokenService.associateTokens.selector,
             account, tokens));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple associations failed!");
     }
 
-    function safeAssociateToken(IHTS token, address account) internal {
+    function safeAssociateToken(IHederaTokenService token, address account) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.associateToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.associateToken.selector,
             account, token));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe single association failed!");
@@ -67,62 +67,62 @@ library SafeHTS {
     function safeDissociateTokens(address account, address[] memory tokens) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.dissociateTokens.selector,
+            abi.encodeWithSelector(IHederaTokenService.dissociateTokens.selector,
             account, tokens));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple dissociations failed!");
     }
 
-    function safeDissociateToken(IHTS token, address account) internal {
+    function safeDissociateToken(IHederaTokenService token, address account) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.dissociateToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.dissociateToken.selector,
             account, token));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe single dissociation failed!");
     }
 
-    function safeTransferTokens(IHTS token, address[] memory accountIds, int64[] memory amounts) internal {
+    function safeTransferTokens(IHederaTokenService token, address[] memory accountIds, int64[] memory amounts) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.transferTokens.selector,
+            abi.encodeWithSelector(IHederaTokenService.transferTokens.selector,
             token, accountIds, amounts));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe tokens transfer failed!");
     }
 
-    function safeTransferNFTs(IHTS token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) internal {
+    function safeTransferNFTs(IHederaTokenService token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.transferNFTs.selector,
+            abi.encodeWithSelector(IHederaTokenService.transferNFTs.selector,
             token, sender, receiver, serialNumber));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe NFTs transfer failed!");
     }
 
-    function safeTransferToken(IHTS token, address sender, address receiver, int64 amount) internal {
+    function safeTransferToken(IHederaTokenService token, address sender, address receiver, int64 amount) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.transferToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.transferToken.selector,
             token, sender, receiver, amount));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe token transfer failed!");
     }
 
-    function safeTransferNFT(IHTS token, address sender, address receiver, int64 serialNumber) internal {
+    function safeTransferNFT(IHederaTokenService token, address sender, address receiver, int64 serialNumber) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHTS.transferNFT.selector,
+            abi.encodeWithSelector(IHederaTokenService.transferNFT.selector,
             token, sender, receiver, serialNumber));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe NFT transfer failed!");
     }
 
-    function safeCreateFungibleToken(IHTS.HederaToken memory token, uint initialTotalSupply,
+    function safeCreateFungibleToken(IHederaTokenService.HederaToken memory token, uint initialTotalSupply,
         uint decimals) nonEmptyExpiry(token) internal returns (address tokenAddress){
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call{value: msg.value}(
-            abi.encodeWithSelector(IHTS.createFungibleToken.selector,
+            abi.encodeWithSelector(IHederaTokenService.createFungibleToken.selector,
             token, initialTotalSupply, decimals));
         (responseCode, tokenAddress) =
         success
@@ -131,15 +131,15 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe create fungible token failed!");
     }
 
-    function safeCreateFungibleTokenWithCustomFees(IHTS.HederaToken memory token,
+    function safeCreateFungibleTokenWithCustomFees(IHederaTokenService.HederaToken memory token,
         uint initialTotalSupply,
         uint decimals,
-        IHTS.FixedFee[] memory fixedFees,
-        IHTS.FractionalFee[] memory fractionalFees) nonEmptyExpiry(token) internal returns
+        IHederaTokenService.FixedFee[] memory fixedFees,
+        IHederaTokenService.FractionalFee[] memory fractionalFees) nonEmptyExpiry(token) internal returns
     (address tokenAddress){
         int responseCode;
         (bool success, bytes memory result) = precompileAddress.call{value: msg.value}(
-            abi.encodeWithSelector(IHTS.createFungibleTokenWithCustomFees.selector,
+            abi.encodeWithSelector(IHederaTokenService.createFungibleTokenWithCustomFees.selector,
             token, initialTotalSupply, decimals, fixedFees, fractionalFees));
         (responseCode, tokenAddress) =
         success
@@ -148,11 +148,11 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe create fungible token with custom fees failed!");
     }
 
-    function safeCreateNonFungibleToken(IHTS.HederaToken memory token) nonEmptyExpiry(token) internal returns
+    function safeCreateNonFungibleToken(IHederaTokenService.HederaToken memory token) nonEmptyExpiry(token) internal returns
     (address tokenAddress){
         int responseCode;
         (bool success, bytes memory result) = precompileAddress.call{value: msg.value}(
-            abi.encodeWithSelector(IHTS.createNonFungibleToken.selector, token));
+            abi.encodeWithSelector(IHederaTokenService.createNonFungibleToken.selector, token));
         (responseCode, tokenAddress) =
         success
         ? abi.decode(result, (int32, address))
@@ -160,13 +160,13 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe create non fungible token failed!");
     }
 
-    function safeCreateNonFungibleTokenWithCustomFees(IHTS.HederaToken memory token,
-        IHTS.FixedFee[] memory fixedFees,
-        IHTS.RoyaltyFee[] memory royaltyFees) nonEmptyExpiry(token) internal returns
+    function safeCreateNonFungibleTokenWithCustomFees(IHederaTokenService.HederaToken memory token,
+        IHederaTokenService.FixedFee[] memory fixedFees,
+        IHederaTokenService.RoyaltyFee[] memory royaltyFees) nonEmptyExpiry(token) internal returns
     (address tokenAddress){
         int responseCode;
         (bool success, bytes memory result) = precompileAddress.call{value: msg.value}(
-            abi.encodeWithSelector(IHTS.createNonFungibleTokenWithCustomFees.selector,
+            abi.encodeWithSelector(IHederaTokenService.createNonFungibleTokenWithCustomFees.selector,
             token, fixedFees, royaltyFees));
         (responseCode, tokenAddress) =
         success
--- safe-hts-precompile/SafeOperations.sol
+++ safe-hts-precompile/SafeOperations.sol
@@ -5,13 +5,13 @@ pragma experimental ABIEncoderV2;
 import "./SafeHTS.sol";
 
 contract SafeOperations {
-    using SafeHTS for IHTS;
+    using SafeHTS for IHederaTokenService;
 
     function safeTokenAssociate(address sender, address tokenAddress) external {
-        IHTS(tokenAddress).safeAssociateToken(sender);
+        IHederaTokenService(tokenAddress).safeAssociateToken(sender);
     }
     function safeTokenDissociate(address sender, address tokenAddress) external {
-        IHTS(tokenAddress).safeDissociateToken(sender);
+        IHederaTokenService(tokenAddress).safeDissociateToken(sender);
     }
     function safeTokensAssociate(address account, address[] memory tokens) external {
         SafeHTS.safeAssociateTokens(account, tokens);
@@ -20,33 +20,33 @@ contract SafeOperations {
         SafeHTS.safeDissociateTokens(account, tokens);
     }
     function safeTokensTransfer(address token, address[] memory accountIds, int64[] memory amounts) external {
-        IHTS(token).safeTransferTokens(accountIds, amounts);
+        IHederaTokenService(token).safeTransferTokens(accountIds, amounts);
     }
     function safeNFTsTransfer(address token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) external {
-        IHTS(token).safeTransferNFTs(sender, receiver, serialNumber);
+        IHederaTokenService(token).safeTransferNFTs(sender, receiver, serialNumber);
     }
     function safeTokenTransfer(address token, address sender, address receiver, int64 amount) external {
-        IHTS(token).safeTransferToken(sender, receiver, amount);
+        IHederaTokenService(token).safeTransferToken(sender, receiver, amount);
     }
     function safeNFTTransfer(address token, address sender, address receiver, int64 serialNum) external {
-        IHTS(token).safeTransferNFT(sender, receiver, serialNum);
+        IHederaTokenService(token).safeTransferNFT(sender, receiver, serialNum);
     }
-    function safeTransferCrypto(IHTS.TokenTransferList[] memory tokenTransfers) external {
+    function safeTransferCrypto(IHederaTokenService.TokenTransferList[] memory tokenTransfers) external {
         SafeHTS.safeCryptoTransfer(tokenTransfers);
     }
     function safeTokenMint(address token, uint64 amount, bytes[] memory metadata) external
     returns (uint64 newTotalSupply, int64[] memory serialNumbers)
     {
-        (newTotalSupply, serialNumbers) = IHTS(token).safeMintToken(amount, metadata);
+        (newTotalSupply, serialNumbers) = IHederaTokenService(token).safeMintToken(amount, metadata);
     }
     function safeTokenBurn(address token, uint64 amount, int64[] memory serialNumbers) external
     returns (uint64 newTotalSupply)
     {
-        (newTotalSupply) = IHTS(token).safeBurnToken(amount, serialNumbers);
+        (newTotalSupply) = IHederaTokenService(token).safeBurnToken(amount, serialNumbers);
     }
 
     function safeCreateOfFungibleToken() external payable returns (address tokenAddress){
-        IHTS.HederaToken memory token;
+        IHederaTokenService.HederaToken memory token;
         token.name = "tokenName";
         token.symbol = "tokenSymbol";
         token.treasury = address(this);
@@ -58,20 +58,20 @@ contract SafeOperations {
         address feeCollector,
         address existingTokenAddress)
     external payable returns (address tokenAddress){
-        IHTS.HederaToken memory token;
+        IHederaTokenService.HederaToken memory token;
         token.name = "tokenName";
         token.symbol = "tokenSymbol";
         token.treasury = address(this);
 
-        IHTS.FixedFee[] memory fixedFees =
+        IHederaTokenService.FixedFee[] memory fixedFees =
         createFixedFeesWithAllTypes(1, existingTokenAddress, feeCollector);
-        IHTS.FractionalFee[] memory fractionalFees =
+        IHederaTokenService.FractionalFee[] memory fractionalFees =
         createSingleFractionalFeeWithLimits(4, 5, 10, 30, true, feeCollector);
         (tokenAddress) = SafeHTS.safeCreateFungibleTokenWithCustomFees(token, 200, 8, fixedFees, fractionalFees);
     }
 
     function safeCreateOfNonFungibleToken() external payable returns (address tokenAddress){
-        IHTS.HederaToken memory token;
+        IHederaTokenService.HederaToken memory token;
         token.name = "tokenName";
         token.symbol = "tokenSymbol";
         token.memo = "memo";
@@ -82,14 +82,14 @@ contract SafeOperations {
     function safeCreateOfNonFungibleTokenWithCustomFees(
         address feeCollector,
         address existingTokenAddress) external payable returns (address tokenAddress){
-        IHTS.HederaToken memory token;
+        IHederaTokenService.HederaToken memory token;
         token.name = "tokenName";
         token.symbol = "tokenSymbol";
         token.memo = "memo";
         token.treasury = address(this);
-        IHTS.RoyaltyFee[] memory royaltyFees =
+        IHederaTokenService.RoyaltyFee[] memory royaltyFees =
         createRoyaltyFeesWithAllTypes(4, 5, 10, existingTokenAddress, feeCollector);
-        (tokenAddress) = SafeHTS.safeCreateNonFungibleTokenWithCustomFees(token, new IHTS.FixedFee[](0), royaltyFees);
+        (tokenAddress) = SafeHTS.safeCreateNonFungibleTokenWithCustomFees(token, new IHederaTokenService.FixedFee[](0), royaltyFees);
     }
 
     function createRoyaltyFeesWithAllTypes(
@@ -98,24 +98,24 @@ contract SafeOperations {
         uint32 amount,
         address tokenId,
         address feeCollector)
-    internal pure returns (IHTS.RoyaltyFee[] memory royaltyFees) {
-        royaltyFees = new IHTS.RoyaltyFee[](3);
-        IHTS.RoyaltyFee memory royaltyFeeWithoutFallback = createRoyaltyFee(numerator, denominator, feeCollector);
-        IHTS.RoyaltyFee memory royaltyFeeWithFallbackHbar = createRoyaltyFeeWithFallbackFee(numerator, denominator, amount, address(0x0), true, feeCollector);
-        IHTS.RoyaltyFee memory royaltyFeeWithFallbackToken = createRoyaltyFeeWithFallbackFee(numerator, denominator, amount, tokenId, false, feeCollector);
+    internal pure returns (IHederaTokenService.RoyaltyFee[] memory royaltyFees) {
+        royaltyFees = new IHederaTokenService.RoyaltyFee[](3);
+        IHederaTokenService.RoyaltyFee memory royaltyFeeWithoutFallback = createRoyaltyFee(numerator, denominator, feeCollector);
+        IHederaTokenService.RoyaltyFee memory royaltyFeeWithFallbackHbar = createRoyaltyFeeWithFallbackFee(numerator, denominator, amount, address(0x0), true, feeCollector);
+        IHederaTokenService.RoyaltyFee memory royaltyFeeWithFallbackToken = createRoyaltyFeeWithFallbackFee(numerator, denominator, amount, tokenId, false, feeCollector);
         royaltyFees[0] = royaltyFeeWithoutFallback;
         royaltyFees[1] = royaltyFeeWithFallbackHbar;
         royaltyFees[2] = royaltyFeeWithFallbackToken;
     }
 
-    function createRoyaltyFee(uint32 numerator, uint32 denominator, address feeCollector) internal pure returns (IHTS.RoyaltyFee memory royaltyFee) {
+    function createRoyaltyFee(uint32 numerator, uint32 denominator, address feeCollector) internal pure returns (IHederaTokenService.RoyaltyFee memory royaltyFee) {
         royaltyFee.numerator = numerator;
         royaltyFee.denominator = denominator;
         royaltyFee.feeCollector = feeCollector;
     }
 
     function createRoyaltyFeeWithFallbackFee(uint32 numerator, uint32 denominator, uint32 amount, address tokenId, bool useHbarsForPayment,
-        address feeCollector) internal pure returns (IHTS.RoyaltyFee memory royaltyFee) {
+        address feeCollector) internal pure returns (IHederaTokenService.RoyaltyFee memory royaltyFee) {
         royaltyFee.numerator = numerator;
         royaltyFee.denominator = denominator;
         royaltyFee.amount = amount;
@@ -124,43 +124,43 @@ contract SafeOperations {
         royaltyFee.feeCollector = feeCollector;
     }
 
-    function createFixedFeesWithAllTypes(uint32 amount, address tokenId, address feeCollector) internal pure returns (IHTS.FixedFee[] memory fixedFees) {
-        fixedFees = new IHTS.FixedFee[](3);
-        IHTS.FixedFee memory fixedFeeForToken = createFixedFeeForToken(amount, tokenId, feeCollector);
-        IHTS.FixedFee memory fixedFeeForHbars = createFixedFeeForHbars(amount*2, feeCollector);
-        IHTS.FixedFee memory fixedFeeForCurrentToken = createFixedFeeForCurrentToken(amount*4, feeCollector);
+    function createFixedFeesWithAllTypes(uint32 amount, address tokenId, address feeCollector) internal pure returns (IHederaTokenService.FixedFee[] memory fixedFees) {
+        fixedFees = new IHederaTokenService.FixedFee[](3);
+        IHederaTokenService.FixedFee memory fixedFeeForToken = createFixedFeeForToken(amount, tokenId, feeCollector);
+        IHederaTokenService.FixedFee memory fixedFeeForHbars = createFixedFeeForHbars(amount*2, feeCollector);
+        IHederaTokenService.FixedFee memory fixedFeeForCurrentToken = createFixedFeeForCurrentToken(amount*4, feeCollector);
         fixedFees[0] = fixedFeeForToken;
         fixedFees[1] = fixedFeeForHbars;
         fixedFees[2] = fixedFeeForCurrentToken;
     }
 
-    function createFixedFeeForToken(uint32 amount, address tokenId, address feeCollector) internal pure returns (IHTS.FixedFee memory fixedFee) {
+    function createFixedFeeForToken(uint32 amount, address tokenId, address feeCollector) internal pure returns (IHederaTokenService.FixedFee memory fixedFee) {
         fixedFee.amount = amount;
         fixedFee.tokenId = tokenId;
         fixedFee.feeCollector = feeCollector;
     }
 
-    function createFixedFeeForHbars(uint32 amount, address feeCollector) internal pure returns (IHTS.FixedFee memory fixedFee) {
+    function createFixedFeeForHbars(uint32 amount, address feeCollector) internal pure returns (IHederaTokenService.FixedFee memory fixedFee) {
         fixedFee.amount = amount;
         fixedFee.useHbarsForPayment = true;
         fixedFee.feeCollector = feeCollector;
     }
 
-    function createFixedFeeForCurrentToken(uint32 amount, address feeCollector) internal pure returns (IHTS.FixedFee memory fixedFee) {
+    function createFixedFeeForCurrentToken(uint32 amount, address feeCollector) internal pure returns (IHederaTokenService.FixedFee memory fixedFee) {
         fixedFee.amount = amount;
         fixedFee.useCurrentTokenForPayment = true;
         fixedFee.feeCollector = feeCollector;
     }
 
     function createSingleFractionalFeeWithLimits(uint32 numerator, uint32 denominator, uint32 minimumAmount, uint32 maximumAmount,
-        bool netOfTransfers,  address feeCollector) internal pure returns (IHTS.FractionalFee[] memory fractionalFees) {
-        fractionalFees = new IHTS.FractionalFee[](1);
-        IHTS.FractionalFee memory fractionalFee = createFractionalFeeWithLimits(numerator, denominator, minimumAmount, maximumAmount, netOfTransfers, feeCollector);
+        bool netOfTransfers,  address feeCollector) internal pure returns (IHederaTokenService.FractionalFee[] memory fractionalFees) {
+        fractionalFees = new IHederaTokenService.FractionalFee[](1);
+        IHederaTokenService.FractionalFee memory fractionalFee = createFractionalFeeWithLimits(numerator, denominator, minimumAmount, maximumAmount, netOfTransfers, feeCollector);
         fractionalFees[0] = fractionalFee;
     }
 
     function createFractionalFeeWithLimits(uint32 numerator, uint32 denominator, uint32 minimumAmount, uint32 maximumAmount,
-        bool netOfTransfers,  address feeCollector) internal pure returns (IHTS.FractionalFee memory fractionalFee) {
+        bool netOfTransfers,  address feeCollector) internal pure returns (IHederaTokenService.FractionalFee memory fractionalFee) {
         fractionalFee.numerator = numerator;
         fractionalFee.denominator = denominator;
         fractionalFee.minimumAmount = minimumAmount;
