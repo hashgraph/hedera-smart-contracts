7a5060bc32b333bae17a8ad4fb204b5e9c38bb8d
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2022-08-19 16:41:33 +0300

    Add HIP-514 functions to SafeHTS library
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- safe-hts-precompile/SafeHTS.sol
+++ safe-hts-precompile/SafeHTS.sol
@@ -175,7 +175,280 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe create non fungible token with custom fees failed!");
     }
 
-    modifier nonEmptyExpiry(IHTS.HederaToken memory token)
+    function safeApprove(IHederaTokenService token, address spender, uint256 amount) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.approve.selector, token, spender, amount));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe approve failed!");
+    }
+
+    function safeAllowance(IHederaTokenService token, address owner, address spender) internal
+    returns (uint256 allowance)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.allowance.selector, token, owner, spender));
+        (responseCode, allowance) = success ? abi.decode(result, (int32, uint256)) : (HederaResponseCodes.UNKNOWN, 0);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe allowance failed!");
+    }
+
+    function safeApproveNFT(IHederaTokenService token, address approved, int64 serialNumber) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.approveNFT.selector, token, approved, serialNumber));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe NFT approve failed!");
+    }
+
+    function safeGetApproved(IHederaTokenService token, int64 serialNumber) internal
+    returns (address approved)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getApproved.selector, token, serialNumber));
+        (responseCode, approved) = success ? abi.decode(result, (int32, address)) : (HederaResponseCodes.UNKNOWN, address(0));
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe get approved failed!");
+    }
+
+    function safeSetApprovalForAll(IHederaTokenService token, address operator, bool approved) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.setApprovalForAll.selector, token, operator, approved));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe set token approval failed!");
+    }
+
+    function safeIsApprovedForAll(IHederaTokenService token, address owner, address operator) internal
+    returns (bool approved)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.isApprovedForAll.selector, token, owner, operator));
+        (responseCode, approved) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for if approved for all failed!");
+    }
+
+    function safeIsFrozen(IHederaTokenService token, address account) internal
+    returns (bool frozen)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.isFrozen.selector, token, account));
+        (responseCode, frozen) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for if frozen failed!");
+    }
+
+    function safeIsKyc(IHederaTokenService token, address account) internal
+    returns (bool kycGranted)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.isKyc.selector, token, account));
+        (responseCode, kycGranted) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for if KYC is granted failed!");
+    }
+
+    function safeDeleteToken(IHederaTokenService token) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.deleteToken.selector, token));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe token delete failed!");
+    }
+
+    function safeGetTokenCustomFees(IHederaTokenService token) internal
+    returns (IHederaTokenService.FixedFee[] memory fixedFees, IHederaTokenService.FractionalFee[] memory fractionalFees, IHederaTokenService.RoyaltyFee[] memory royaltyFees)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenCustomFees.selector, token));
+        (responseCode, fixedFees, fractionalFees, royaltyFees) =
+        success
+        ? abi.decode(result, (int32, IHederaTokenService.FixedFee[], IHederaTokenService.FractionalFee[], IHederaTokenService.RoyaltyFee[]))
+        : (HederaResponseCodes.UNKNOWN, fixedFees, fractionalFees, royaltyFees);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token custom fees failed!");
+    }
+
+    function safeGetTokenDefaultFreezeStatus(IHederaTokenService token) internal
+    returns (bool defaultFreezeStatus)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenDefaultFreezeStatus.selector, token));
+        (responseCode, defaultFreezeStatus) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token default freeze status failed!");
+    }
+
+    function safeGetTokenDefaultKycStatus(IHederaTokenService token) internal
+    returns (bool defaultKycStatus)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenDefaultKycStatus.selector, token));
+        (responseCode, defaultKycStatus) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token default KYC status failed!");
+    }
+
+    function safeGetTokenExpiryInfo(IHederaTokenService token) internal
+    returns (IHederaTokenService.Expiry memory expiry)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenExpiryInfo.selector, token));
+        (responseCode, expiry) = success ? abi.decode(result, (int32, IHederaTokenService.Expiry)) : (HederaResponseCodes.UNKNOWN, expiry);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token expiry info failed!");
+    }
+
+    function safeGetFungibleTokenInfo(IHederaTokenService token) internal
+    returns (IHederaTokenService.FungibleTokenInfo memory fungibleTokenInfo)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getFungibleTokenInfo.selector, token));
+        (responseCode, fungibleTokenInfo) = success ? abi.decode(result, (int32, IHederaTokenService.FungibleTokenInfo)) : (HederaResponseCodes.UNKNOWN, fungibleTokenInfo);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for fungible token info failed!");
+    }
+
+    function safeGetTokenInfo(IHederaTokenService token) internal
+    returns (IHederaTokenService.TokenInfo memory tokenInfo)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenInfo.selector, token));
+        (responseCode, tokenInfo) = success ? abi.decode(result, (int32, IHederaTokenService.TokenInfo)) : (HederaResponseCodes.UNKNOWN, tokenInfo);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token info failed!");
+    }
+
+    function safeGetTokenKey(IHederaTokenService token, uint keyType) internal
+    returns (IHederaTokenService.KeyValue memory key)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenKey.selector, token, keyType));
+        (responseCode, key) = success ? abi.decode(result, (int32, IHederaTokenService.KeyValue)) : (HederaResponseCodes.UNKNOWN, key);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token key failed!");
+    }
+
+    function safeGetNonFungibleTokenInfo(IHederaTokenService token, int64 serialNumber) internal
+    returns (IHederaTokenService.NonFungibleTokenInfo memory nonFungibleTokenInfo)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getNonFungibleTokenInfo.selector, token, serialNumber));
+        (responseCode, nonFungibleTokenInfo) = success ? abi.decode(result, (int32, IHederaTokenService.NonFungibleTokenInfo)) : (HederaResponseCodes.UNKNOWN, nonFungibleTokenInfo);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for NFT info failed!");
+    }
+
+    function safeFreezeToken(IHederaTokenService token, address account) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.freezeToken.selector, token, account));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe freeze token operation failed!");
+    }
+
+    function safeUnfreezeToken(IHederaTokenService token, address account) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.unfreezeToken.selector, token, account));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe unfreeze token operation failed!");
+    }
+
+    function safeGrantTokenKyc(IHederaTokenService token, address account) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.grantTokenKyc.selector, token, account));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe grant token KYC operation failed!");
+    }
+
+    function safeRevokeTokenKyc(IHederaTokenService token, address account) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.revokeTokenKyc.selector, token, account));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe revoke token KYC operation failed!");
+    }
+
+    function safePauseToken(IHederaTokenService token) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.pauseToken.selector, token));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe pause token operation failed!");
+    }
+
+    function safeUnpauseToken(IHederaTokenService token) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.unpauseToken.selector, token));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe unpause token operation failed!");
+    }
+
+    function safeWipeTokenAccount(IHederaTokenService token, address account, uint32 amount) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.wipeTokenAccount.selector, token, account, amount));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe wipe token account operation failed!");
+    }
+
+    function safeWipeTokenAccountNFT(IHederaTokenService token, address account, int64[] memory serialNumbers) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.wipeTokenAccountNFT.selector, token, account, serialNumbers));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe wipe token account NFT operation failed!");
+    }
+
+    function safeUpdateTokenInfo(IHederaTokenService token, IHederaTokenService.HederaToken memory tokenInfo) nonEmptyExpiry(tokenInfo) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.updateTokenInfo.selector, token, tokenInfo));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe update token info operation failed!");
+    }
+
+    function safeUpdateTokenExpiryInfo(IHederaTokenService token, IHederaTokenService.Expiry memory expiryInfo) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.updateTokenExpiryInfo.selector, token, expiryInfo));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe update token expiry info operation failed!");
+    }
+
+    function safeUpdateTokenKeys(IHederaTokenService token, IHederaTokenService.TokenKey[] memory keys) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.updateTokenKeys.selector, token, keys));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe update token keys operation failed!");
+    }
+
+    function safeIsToken(IHederaTokenService token) internal
+    returns (bool isToken)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.isToken.selector, token));
+        (responseCode, isToken) = success ? abi.decode(result, (int32, bool)) : (HederaResponseCodes.UNKNOWN, false);
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for if valid token at address failed!");
+    }
+
+    function safeGetTokenType(IHederaTokenService token) internal
+    returns (int32 tokenType)
+    {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHederaTokenService.getTokenType.selector, token));
+        (responseCode, tokenType) = success ? abi.decode(result, (int32, int32)) : (HederaResponseCodes.UNKNOWN, int32(0));
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe query for token type failed!");
+    }
+
+    modifier nonEmptyExpiry(IHederaTokenService.HederaToken memory token)
     {
         if (token.expiry.second == 0 && token.expiry.autoRenewPeriod == 0) {
             token.expiry.autoRenewPeriod = defaultAutoRenewPeriod;
