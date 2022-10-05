c61cc9f958bfa1721045a9426670e51b04a5f9b9
Ivo Yankov <ivo@devlabs.bg>
2022-10-05 08:43:44 +0300

    Test CI (#76)
    * chore: fix existing safeHTS tests
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * feat: add test ci
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: tweak the ci
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: tweak the ci
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: tweak the ci
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * fix: update chainId in services config for CI
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: tweak the CI
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: separate CI tasks
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,8 @@ module.exports = {
       url: 'http://localhost:7546',
       accounts: [
         "0x2e1d968b041d84dd120a5860cee60cd83f9374ef527ca86996317ada3d0d03e7"
-      ]
+      ],
+      chainId: 298,
     }
   }
 };
