30404a3f1ed06031558c343ea1f12391f1d60d0a
Nikolay Atanasow <n.atanasow94@gmail.com>
2022-10-07 14:11:53 +0300

    Use HTS NFTs as if they're deployed ERC721/IERC721Metadata/IERC721Enumerable contracts (#82)
    * Add tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Install @openzeppelin/contracts
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Bump hedera local node version
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Fix typos
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit workflow
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add tmate for debugging
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,9 @@ require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  mocha: {
+    timeout: 3600000
+  },
   solidity: {
     version: "0.8.9",
     settings: {
@@ -16,7 +19,8 @@ module.exports = {
     relay: {
       url: 'http://localhost:7546',
       accounts: [
-        "0x2e1d968b041d84dd120a5860cee60cd83f9374ef527ca86996317ada3d0d03e7"
+        "0x2e1d968b041d84dd120a5860cee60cd83f9374ef527ca86996317ada3d0d03e7",
+        "0x45a5a7108a18dd5013cf2d5857a28144beadc9c70b3bdbd914e38df4e804b8d8"
       ],
       chainId: 298,
     }
