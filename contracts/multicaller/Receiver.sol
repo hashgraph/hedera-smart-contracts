b228a2e951d30dfcb25d5b00337384fed06ec23e
Ivo Yankov <ivo@devlabs.bg>
2023-02-07 11:52:31 +0200

    Multicaller tests (#168)
    * feat: add multicaller contract and tests
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * feat: add multicaller tests for txs
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: cleanup
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * fix: assert output length via events
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: add compiled contracts
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * refactor: use Multicall3 contract
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: improve code readability
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    ---------
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

---
+++ contracts/multicaller/Receiver.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Receiver {
+
+    uint public counter = 0;
+    event Counter(uint);
+
+    struct SomeData {
+        uint a;
+        uint b;
+        uint c;
+        uint d;
+    }
+
+    function processLongInput(SomeData memory longInput) pure external returns (uint result) {
+        result = 5;
+    }
+
+    function processLongOutput(
+        uint24 count
+    ) external pure returns (SomeData[] memory) {
+        SomeData[] memory data = new SomeData[](count);
+        return data;
+    }
+
+    function processLongInputTx(SomeData memory longInput) payable external returns (uint) {
+        counter += 1;
+        return counter;
+    }
+
+    function processLongOutputTx(
+        uint24 count
+    ) external payable returns (SomeData[] memory) {
+        counter += 1;
+        SomeData[] memory data = new SomeData[](count);
+        emit Counter(counter);
+        return data;
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+}
