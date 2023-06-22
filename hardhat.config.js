044903209f6917aaf3824227d9197b2ea8ebb241
Nikolay Atanasow <n.atanasow94@gmail.com>
2023-06-23 02:07:05 +0300

    Update contracts.maxNumWithHapiSigsAccess to 0 and update test (#245)
    * Edit tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Bump local node version
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit versions
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add debug
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit versions
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Fix tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Fix IERC20 tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * FIx token management tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add timeouts
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit token create contract
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Bump network node version
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * add hardhat config for testnet and previewnet
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    
    * Remove getSDKClientOperator function from utils
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    
    * Update functions in Utils
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    
    * Remove duplicate variables
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    
    ---------
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Co-authored-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,6 +38,43 @@ module.exports = {
         "0x45a5a7108a18dd5013cf2d5857a28144beadc9c70b3bdbd914e38df4e804b8d8",
       ],
       chainId: 298,
+      sdkClient: {
+        operatorId: '0.0.2',
+        operatorKey: '302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137',
+        networkNodeUrl: '127.0.0.1:50211',
+        nodeId: '3',
+        mirrorNode: '127.0.0.1:5600'
+      }
     },
-  },
+    testnet: {
+      url: "https://testnet.hashio.io/api",
+      accounts: [
+        "0x2e1d968b041d84dd120a5860cee60cd83f9374e0000000000000000000000000", //Add valid account for testnet
+        "0x45a5a7108a18dd5013cf2d5857a28144beadc9c0000000000000000000000000", //Add valid account for testnet
+      ],
+      chainId: 296,
+      sdkClient: {
+        operatorId: '', //Format 0.0.ID
+        operatorKey: '302e...',
+        networkNodeUrl: 'https://testnet.hedera.com',
+        nodeId: '3',
+        mirrorNode: 'https://testnet.mirrornode.hedera.com'
+      }
+    },
+    previewnet: {
+      url: "https://previewnet.hashio.io/api",
+      accounts: [
+        "0x2e1d968b041d84dd120a5860cee60cd83f9374e0000000000000000000000000", //Add valid account for previewnet
+        "0x45a5a7108a18dd5013cf2d5857a28144beadc9c0000000000000000000000000", //Add valid account for previewnet
+      ],
+      chainId: 297,
+      sdkClient: {
+        operatorId: '', //Format 0.0.ID
+        operatorKey: '302e...',
+        networkNodeUrl: 'https://previewnet.hedera.com',
+        nodeId: '3',
+        mirrorNode: 'https://previewnet.mirrornode.hedera.com'
+      }
+    },
+  }
 };
