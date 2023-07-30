36b4962c7648b6d15ffcff0d7bc86700489a9ff6
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-07-30 16:48:55 -0500

    dapp-feat: exported basic metadata to root layout
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- system-contract-dapp-playground/src/app/layout.tsx
+++ system-contract-dapp-playground/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import dappMetadata from '@/utils/metadata';
 
 /** @notice Root Layout */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -8,3 +9,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
+/** @notice export metadata for SEO */
+export const metadata = dappMetadata;
