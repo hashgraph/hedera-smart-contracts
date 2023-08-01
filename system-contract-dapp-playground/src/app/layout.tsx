3267260ebb1e5508ac279b55cfe76b10a5198444
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-01 14:14:04 -0500

    dapp-feat: created and added background gradients to root layout
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- system-contract-dapp-playground/src/app/layout.tsx
+++ system-contract-dapp-playground/src/app/layout.tsx
@@ -22,13 +22,21 @@ import '@/styles/globals.css';
 import StyreneAWebFont from '@/fonts';
 import dappMetadata from '@/utils/metadata';
 import ChakraUIProviders from '@/libs/chakra/provider';
+import BgGradient from '@/components/background-gradients';
 
 /** @notice Root Layout */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${StyreneAWebFont.className}`}>
+    <html lang="en" className={`${StyreneAWebFont.variable}`}>
       <body>
-        <ChakraUIProviders>{children}</ChakraUIProviders>
+        <ChakraUIProviders>
+          <main className="bg-primary overflow-hidden font-styrene">
+            <div className="relative h-screen 2xl:max-w-[100rem] 2xl:mx-auto">
+              {children}
+              <BgGradient />
+            </div>
+          </main>
+        </ChakraUIProviders>
       </body>
     </html>
   );
