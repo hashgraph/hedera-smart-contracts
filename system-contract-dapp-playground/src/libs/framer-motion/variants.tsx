9a71c03147f14448cd561b058c9786fb6d31fc80
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-03 13:48:16 -0500

    dapp-feat: added navbar component to root
    - added hedera-logomark.svg
    - created navVariants for animation
    - created navbar component for landing page
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- system-contract-dapp-playground/src/libs/framer-motion/variants.tsx
+++ system-contract-dapp-playground/src/libs/framer-motion/variants.tsx
@@ -17,12 +17,26 @@
  * limitations under the License.
  *
  */
-import Navbar from '@/components/navbar';
 
-export default function Home() {
-  return (
-    <main className="bg-primary overflow-hidden relative h-screen flex flex-col">
-      <Navbar />
-    </main>
-  );
-}
+import { Variants } from 'framer-motion';
+
+export const navVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: -30,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 0.6,
+    },
+  },
+};
