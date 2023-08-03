9ddae2f39d8001f9d282419edc9a99c672ef2e93
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-03 13:55:26 -0500

    dapp-feat: created footer component
    - added HEDERA_SOCIAL_MEDIA constant variable which holds all hedera social media information
    
    - created and added new footer component to root layout
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ system-contract-dapp-playground/src/utils/constants.ts
@@ -0,0 +1,30 @@
+export const HEDERA_SOCIAL_MEDIA = [
+  {
+    name: 'discord',
+    link: 'https://discord.com/invite/hedera',
+  },
+  {
+    name: 'facebook',
+    link: 'https://www.facebook.com/hashgraph',
+  },
+  {
+    name: 'linkedin',
+    link: 'https://www.linkedin.com/company/hashgraph/',
+  },
+  {
+    name: 'reddit',
+    link: 'https://www.reddit.com/r/Hedera/',
+  },
+  {
+    name: 'telegram',
+    link: 'https://t.me/hederahashgraph',
+  },
+  {
+    name: 'twitter',
+    link: 'https://twitter.com/hedera',
+  },
+  {
+    name: 'youtube',
+    link: 'https://www.youtube.com/hederahashgraph',
+  },
+];
