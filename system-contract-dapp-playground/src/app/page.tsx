f70d2630430eb509821fc563c9fc784ba2dcbdd7
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-03 13:50:22 -0500

    dapp-feat: added hero landing page component to root
    - created common vertical framer-motion variant object
    - created LandingPage component
    - added LandingPage to root layout
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- system-contract-dapp-playground/src/app/page.tsx
+++ system-contract-dapp-playground/src/app/page.tsx
@@ -18,11 +18,13 @@
  *
  */
 import Navbar from '@/components/navbar';
+import LandingPage from '@/sections/landing';
 
 export default function Home() {
   return (
     <main className="bg-primary overflow-hidden relative h-screen flex flex-col">
       <Navbar />
+      <LandingPage />
     </main>
   );
 }
--- system-contract-dapp-playground/src/libs/framer-motion/variants.tsx
+++ system-contract-dapp-playground/src/libs/framer-motion/variants.tsx
@@ -40,3 +40,31 @@ export const navVariants: Variants = {
     },
   },
 };
+
+export const VerticalCommonVariants = (
+  initY: number,
+  initDuration?: number,
+  initStaggerChildren?: number
+): Variants => {
+  return {
+    hidden: {
+      opacity: 0,
+      y: initY,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 140,
+      },
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: initDuration ? initDuration : 0.3,
+        staggerChildren: initStaggerChildren ? initStaggerChildren : 0.13,
+        type: 'spring',
+        stiffness: 80,
+      },
+    },
+  };
+};
