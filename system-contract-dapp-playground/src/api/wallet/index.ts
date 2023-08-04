c952b88e8aaca54a4fcfcf5746eca6bdf2d96372
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-04 16:53:29 -0500

    feat: added connect wallet feature to Dapp (#273)
    * dapp-docs: added js-cookie and ethers packages
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added customized Toaster component
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-docs: added license header to constants.js
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added WalletResult interface
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added HEDERA_NETWORKS constant which contains information about Hedera networks
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added basic Metamask APIs
    
    - getEthereumObject(): returns Ethereum object
    
    - getWalletProvider(): retrieve wallet from browser provider
    
    - getCurrentChainId(): retrieve current network ID
    
    - isCorrectHederaNetwork(): detect if the current network
    is expected Hedera network
    
    - requestAccount(): connect account
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added APIs to interact with client Cookies
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: created new Overview protected route
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added a middleware to handle authenticating protected routes
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-feat: added connect wallet feature to landing page
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-chore: renamed API methods, replaced Ethereum with Hedera
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-chore: updated hrc-719 protected route's name
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    ---------
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    Signed-off-by: Mo Shaikjee <shaikjeemohammed@gmail.com>

---
+++ system-contract-dapp-playground/src/api/wallet/index.ts
@@ -0,0 +1,111 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { WalletResult } from '@/types/interfaces';
+import { HEDERA_NETWORKS } from '@/utils/constants';
+import { ethers, BrowserProvider } from 'ethers';
+
+/**
+ * @dev get wallet object if available
+ *
+ * @return object<any>
+ */
+export const getWalletObject = () => {
+  if (typeof window !== 'undefined') {
+    const { ethereum }: any = window;
+    return ethereum;
+  }
+};
+
+/**
+ * @dev get ethersjs wallet provider (i.e. Metamask provider)
+ *
+ * @return WalletResult
+ */
+export const getWalletProvider = (): WalletResult => {
+  // prepare walletObject
+  const walletObject = getWalletObject();
+  if (!walletObject) {
+    return { err: '!HEDERA' };
+  }
+
+  // get walletProvider
+  const walletProvider: BrowserProvider = new ethers.BrowserProvider(walletObject);
+  return { walletProvider };
+};
+
+/**
+ * @dev return current chainId of the network that the walletPro is connected to
+ *
+ * @params walletProvider: ethers.BrowserProvider
+ *
+ * @returns Promise<WalletResult>
+ */
+export const getCurrentChainId = async (
+  walletProvider: ethers.BrowserProvider
+): Promise<WalletResult> => {
+  try {
+    const currentChainId = await walletProvider.send('eth_chainId', []);
+    return {
+      currentChainId,
+    };
+  } catch (err) {
+    return { err };
+  }
+};
+
+/**
+ * @dev Handles checking if the connected network is the expected network (i.e. HEDERA_TESTNET, HEDERA_PREVIEWNET, HEDERA_LOCALNET, HEDERA_MAINNET)
+ *
+ * @params walletProvider: ethers.BrowserProvider
+ *
+ * @returns bool
+ */
+export const isCorrectHederaNetwork = async (walletProvider: ethers.BrowserProvider) => {
+  // get current chainId
+  const currentChainId = (await getCurrentChainId(walletProvider)).currentChainId as string;
+
+  return (
+    currentChainId === HEDERA_NETWORKS.mainnet.chainIdHex ||
+    currentChainId === HEDERA_NETWORKS.testnet.chainIdHex ||
+    currentChainId === HEDERA_NETWORKS.previewnet.chainIdHex ||
+    currentChainId === HEDERA_NETWORKS.localnet.chainIdHex
+  );
+};
+
+/**
+ * @dev requests a list of connected accounts in a the wallet
+ *
+ * @params walletProvider: ethers.BrowserProvider
+ *
+ * @returns Promise<WalletResult>
+ */
+export const requestAccount = async (
+  walletProvider: ethers.BrowserProvider
+): Promise<WalletResult> => {
+  try {
+    const accounts: [string] = await walletProvider.send('eth_requestAccounts', []);
+    return {
+      accounts,
+    };
+  } catch (err) {
+    return { err };
+  }
+};
