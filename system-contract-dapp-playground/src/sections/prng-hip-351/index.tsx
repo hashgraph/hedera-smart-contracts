770398b58c0edc8b13fdf3dc48a816ce2c170d98
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-09 18:35:43 -0500

    dapp-feat: added new section for prng-hip-351 route
    - added new section UI for prng-hip-351 route
    - added deployment flow for PrngSystemContract contract
    - added deployed details for PrngSystemContract contract
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ system-contract-dapp-playground/src/sections/prng-hip-351/index.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { BsGithub } from 'react-icons/bs';
+import { Tooltip } from '@chakra-ui/react';
+import { IoOpenOutline } from 'react-icons/io5';
+import ContractInteraction from '@/components/contract-interaction';
+import { HEDERA_OFFICIAL_HIPS_URL, HEDERA_SMART_CONTRACTS_ASSETS } from '@/utils/constants';
+
+const HIP351Section = () => {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.6,
+      }}
+      viewport={{ once: true }}
+      className="text-white w-full flex pt-7 pb-12 pl-7 flex-col gap-9"
+    >
+      {/* top part */}
+      <div className="flex flex-col gap-6">
+        {/* TITLE */}
+        <h1 className="text-[1.88rem] font-medium leading-10 flex gap-1 whitespace-nowrap">
+          Pseudo random number System Contract Wrapper
+          <div className="flex">
+            (
+            <Link
+              href={`${HEDERA_OFFICIAL_HIPS_URL}/hip/hip-351`}
+              target="_blank"
+              className="hover:underline flex gap-1 items-center whitespace-nowrap"
+            >
+              <p>HIP-351</p>
+              <div className="text-2xl">
+                <IoOpenOutline />
+              </div>
+            </Link>
+            )
+          </div>
+        </h1>
+
+        {/* content */}
+        <div>
+          {/* Overview */}
+          <div className="text-[1.65rem] font-medium">Overview</div>
+
+          {/* content */}
+          <div className="tracking-tight text-white/70">
+            A Solidity precompiled contracts that generates pseuodrandom numbers for use by both
+            smart contracts and users.
+          </div>
+        </div>
+      </div>
+
+      {/* Contract */}
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-6 w-full">
+          {/* title */}
+          <div className="flex gap-1 items-center text-[20px]">
+            {/* title */}
+            <p>{HEDERA_SMART_CONTRACTS_ASSETS.PRNG_PRECOMPILED.title}</p>
+
+            {/* Github icon */}
+            <Tooltip label="Visit source code on Github." placement={'top'}>
+              <Link
+                href={HEDERA_SMART_CONTRACTS_ASSETS.PRNG_PRECOMPILED.githubUrl}
+                target="_blank"
+                className="text-2xl"
+              >
+                <BsGithub />
+              </Link>
+            </Tooltip>
+          </div>
+
+          {/* contract interaction component */}
+          <ContractInteraction contract={HEDERA_SMART_CONTRACTS_ASSETS.PRNG_PRECOMPILED} />
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default HIP351Section;
