56158ffe1f9c6996a6169ce117bea4627c45f9e8
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-08-30 20:44:57 -0500

    Done
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    dapp-feat: added more interfaces which adhere to IHederaTokenService structs
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    dapp-feat: added a suites of API methods to interact with TokenTransfer contract
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ system-contract-dapp-playground/__tests__/hedera/hts-interactions/token-transfer-contract/index.test.ts
@@ -0,0 +1,420 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {
+  transferCrypto,
+  transferFungibleTokens,
+  transferNonFungibleTokens,
+  transferSingleToken,
+  transferSingleTokenFrom,
+} from '@/api/hedera/tokenTransfer-interactions';
+import {
+  IHederaTokenServiceTokenTransferList,
+  IHederaTokenServiceTransferList,
+} from '@/types/contract-interactions/HTS';
+import { Contract } from 'ethers';
+
+describe('TokenTransferContract test suite', () => {
+  const responseCode = 22;
+  const invalidSender = '0xabc';
+  const senderA = '0xDd7fCb7c2ee96A79B1e201d25F5E43d6a0cED5e6';
+  const senderB = '0x0851072d7bB726305032Eff23CB8fd22eB74c85B';
+  const receiverA = '0x7a35433804d8Cd070d98d66C6E9b45c6C32C3CDD';
+  const receiverB = '0x9de0881b3110aA8cAD1dF3182B1eB6F14d1608a2';
+  const accountAddress = '0x34810E139b451e0a4c67d5743E956Ac8990842A8';
+  const hederaTokenAddress = '0x00000000000000000000000000000000000084b7';
+  const txHash = '0x63424020a69bf46a0669f46dd66addba741b9c02d37fab1686428f5209bc759d';
+
+  const contractMockedResolvedValue = {
+    wait: jest.fn().mockResolvedValue({
+      logs: [
+        {
+          fragment: {
+            name: 'ResponseCode',
+          },
+          data: responseCode,
+        },
+      ],
+      hash: txHash,
+    }),
+  };
+
+  // mock baseContract object
+  const baseContract = {
+    cryptoTransferPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferTokensPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferNFTsPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferTokenPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferNFTPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferFromPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+    transferFromNFTPublic: jest.fn().mockResolvedValue(contractMockedResolvedValue),
+  };
+
+  describe('transferCrypto test suite', () => {
+    // prepare transferList:IHederaTokenServiceTransferList param
+    const transfers = [
+      {
+        accountID: senderA,
+        amount: 369,
+        isApproval: true,
+      },
+    ];
+    const transferList: IHederaTokenServiceTransferList = {
+      transfers,
+    };
+
+    // prepare tokenTransferList: IHederaTokenServiceTokenTransferList
+    const nftTransfers = [
+      {
+        senderAcocuntID: senderA,
+        receiverAccountID: receiverA,
+        serialNumber: 3,
+        isApproval: false,
+      },
+    ];
+    const tokenTransferList: IHederaTokenServiceTokenTransferList = {
+      token: hederaTokenAddress,
+      transfers,
+      nftTransfers,
+    };
+
+    it('should execute transferCrypto then return a successful response code', async () => {
+      const txRes = await transferCrypto(
+        baseContract as unknown as Contract,
+        transferList,
+        tokenTransferList
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+  });
+
+  describe('transferFungibleTokens test suite', () => {
+    it('should execute transferFungibleTokens then return a successful response code', async () => {
+      const txRes = await transferFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, senderB],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferFungibleTokens with an invalid token address then return an error', async () => {
+      const txRes = await transferFungibleTokens(
+        baseContract as unknown as Contract,
+        '0xabc',
+        [senderA, senderB],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBe('Invalid token address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferFungibleTokens with an invalid sender ID then return an error', async () => {
+      const txRes = await transferFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, '0xabc'],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBe(`${invalidSender} is an invalid accountID`);
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferFungibleTokens with an invalid amount then return an error', async () => {
+      const txRes = await transferFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, senderB],
+        [-3, 6, 9]
+      );
+
+      expect(txRes.err).toBe(`-3 is an invalid amount`);
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+  });
+
+  describe('transferNonFungibleTokens test suite', () => {
+    it('should execute transferNonFungibleTokens then return a successful response code', async () => {
+      const txRes = await transferNonFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, senderB],
+        [receiverA, receiverB],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferNonFungibleTokens with an invalid token address then return an error', async () => {
+      const txRes = await transferNonFungibleTokens(
+        baseContract as unknown as Contract,
+        '0xabc',
+        [senderA, senderB],
+        [receiverA, receiverB],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBe('Invalid token address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferNonFungibleTokens with an invalid sender ID then return an error', async () => {
+      const txRes = await transferNonFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, '0xabc'],
+        [receiverA, receiverB],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBe(`${invalidSender} is an invalid sender accountID`);
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferNonFungibleTokens with an invalid receiver ID then return an error', async () => {
+      const txRes = await transferNonFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, senderB],
+        [receiverA, '0xabc'],
+        [3, 6, 9]
+      );
+
+      expect(txRes.err).toBe(`${invalidSender} is an invalid receiver accountID`);
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferNonFungibleTokens with an invalid amount then return an error', async () => {
+      const txRes = await transferNonFungibleTokens(
+        baseContract as unknown as Contract,
+        hederaTokenAddress,
+        [senderA, senderB],
+        [receiverA, receiverB],
+        [-3, 6, 9]
+      );
+
+      expect(txRes.err).toBe(`-3 is an invalid serial number`);
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+  });
+
+  describe('transferSingleToken test suite', () => {
+    it('should execute transferSingleToken with API === "FUNGIBLE" then return a successful response code', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferSingleToken with API === "NON_FUNGIBLE" then return a successful response code', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'NON_FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferSingleToken with an invalid token address then return an error', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        '0xabc',
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid token address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleToken with an invalid sender accountID then return an error', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        '0xabc',
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid sender address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleToken with an invalid receiver accountID then return an error', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        '0xabc',
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid receiver address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleToken with an invalid quantity then return an error', async () => {
+      const txRes = await transferSingleToken(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverB,
+        -369
+      );
+
+      expect(txRes.err).toBe('Invalid quantity');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+  });
+
+  describe('transferSingleTokenFrom test suite', () => {
+    it('should execute transferSingleTokenFrom with API === "FUNGIBLE" then return a successful response code', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferSingleTokenFrom with API === "NON_FUNGIBLE" then return a successful response code', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'NON_FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBeNull;
+      expect(txRes.result).toBe(true);
+      expect(txRes.transactionHash).toBe(txHash);
+    });
+
+    it('should execute transferSingleTokenFrom with an invalid token address then return an error', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        '0xabc',
+        senderA,
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid token address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleTokenFrom with an invalid sender accountID then return an error', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        '0xabc',
+        receiverA,
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid sender address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleTokenFrom with an invalid receiver accountID then return an error', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        '0xabc',
+        369
+      );
+
+      expect(txRes.err).toBe('Invalid receiver address');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+
+    it('should execute transferSingleTokenFrom with an invalid quantity then return an error', async () => {
+      const txRes = await transferSingleTokenFrom(
+        baseContract as unknown as Contract,
+        'FUNGIBLE',
+        hederaTokenAddress,
+        senderA,
+        receiverB,
+        -369
+      );
+
+      expect(txRes.err).toBe('Invalid quantity');
+      expect(txRes.result).toBeNull;
+      expect(txRes.transactionHash).toBeNull;
+    });
+  });
+});
---
+++ system-contract-dapp-playground/src/api/hedera/tokenTransfer-interactions/index.ts
@@ -0,0 +1,369 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { Contract, isAddress } from 'ethers';
+import {
+  IHederaTokenServiceTransferList,
+  IHederaTokenServiceTokenTransferList,
+  TokenTransferSmartContractResult,
+} from '@/types/contract-interactions/HTS';
+
+/**
+ * @dev transfers Hedera Cryptos
+ *
+ * @dev integrates TokenTransferContract.cryptoTransferPublic()
+ *
+ * @param baseContract: ethers.Contract
+ *
+ * @param transferList: IHederaTokenServiceTransferList
+ *
+ * @param tokenTransferList: IHederaTokenServiceTokenTransferList
+ *
+ * @return Promise<TokenTransferSmartContractResult>
+ */
+export const transferCrypto = async (
+  baseContract: Contract,
+  transferList: IHederaTokenServiceTransferList,
+  tokenTransferList: IHederaTokenServiceTokenTransferList
+): Promise<TokenTransferSmartContractResult> => {
+  // invoking contract methods
+  try {
+    const tx = await baseContract.cryptoTransferPublic(transferList, tokenTransferList);
+
+    const txReceipt = await tx.wait();
+
+    // retrieve responseCode from event
+    const { data } = txReceipt.logs.filter(
+      (event: any) => event.fragment.name === 'ResponseCode'
+    )[0];
+
+    // @notice: 22 represents the predefined response code from the Hedera system contracts, indicating a successful transaction.
+    return { result: Number(data) === 22, transactionHash: txReceipt.hash };
+  } catch (err: any) {
+    console.error(err);
+    return { err, transactionHash: err.receipt && err.receipt.hash };
+  }
+};
+
+/**
+ * @dev transfers Hedera fungible tokens
+ *
+ * @dev integrates TokenTransferContract.transferTokensPublic()
+ *
+ * @param baseContract: ethers.Contract
+ *
+ * @param hederaTokenAddress: string
+ *
+ * @param accountId: string[]
+ *
+ * @param amount: number[]
+ *
+ * @return Promise Promise<TokenTransferSmartContractResult>
+ */
+export const transferFungibleTokens = async (
+  baseContract: Contract,
+  hederaTokenAddress: string,
+  accountIDs: string[],
+  amounts: number[]
+): Promise<TokenTransferSmartContractResult> => {
+  // sanitize params
+  let sanitizeErr;
+  if (!isAddress(hederaTokenAddress)) {
+    sanitizeErr = 'Invalid token address';
+  }
+  if (!sanitizeErr) {
+    accountIDs.some((address) => {
+      if (!isAddress(address)) {
+        sanitizeErr = `${address} is an invalid accountID`;
+        return true;
+      }
+    });
+  }
+  if (!sanitizeErr) {
+    amounts.some((amount) => {
+      if (amount < 0) {
+        sanitizeErr = `${amount} is an invalid amount`;
+        return true;
+      }
+    });
+  }
+  if (sanitizeErr) {
+    console.error(sanitizeErr);
+    return { err: sanitizeErr };
+  }
+
+  // invoking contract methods
+  try {
+    const tx = await baseContract.transferTokensPublic(hederaTokenAddress, accountIDs, amounts);
+
+    const txReceipt = await tx.wait();
+
+    // retrieve responseCode from event
+    const { data } = txReceipt.logs.filter(
+      (event: any) => event.fragment.name === 'ResponseCode'
+    )[0];
+
+    // @notice: 22 represents the predefined response code from the Hedera system contracts, indicating a successful transaction.
+    return { result: Number(data) === 22, transactionHash: txReceipt.hash };
+  } catch (err: any) {
+    console.error(err);
+    return { err, transactionHash: err.receipt && err.receipt.hash };
+  }
+};
+
+/**
+ * @dev transfers Hedera non-fungible tokens
+ *
+ * @dev integrates TokenTransferContract.transferNFTsPublic()
+ *
+ * @param baseContract: ethers.Contract
+ *
+ * @param hederaTokenAddress: string
+ *
+ * @param senders: string[]
+ *
+ * @param receivers: string[]
+ *
+ * @param serialNumbers: number[]
+ *
+ * @return Promise<TokenTransferSmartContractResult>
+ */
+export const transferNonFungibleTokens = async (
+  baseContract: Contract,
+  hederaTokenAddress: string,
+  senders: string[],
+  receivers: string[],
+  serialNumbers: number[]
+): Promise<TokenTransferSmartContractResult> => {
+  // sanitize params
+  let sanitizeErr;
+  if (!isAddress(hederaTokenAddress)) {
+    sanitizeErr = 'Invalid token address';
+  }
+  if (!sanitizeErr) {
+    senders.some((address) => {
+      if (!isAddress(address)) {
+        sanitizeErr = `${address} is an invalid sender accountID`;
+        return true;
+      }
+    });
+  }
+  if (!sanitizeErr) {
+    receivers.some((address) => {
+      if (!isAddress(address)) {
+        sanitizeErr = `${address} is an invalid receiver accountID`;
+        return true;
+      }
+    });
+  }
+  if (!sanitizeErr) {
+    serialNumbers.some((seriNum) => {
+      if (seriNum < 0) {
+        sanitizeErr = `${seriNum} is an invalid serial number`;
+        return true;
+      }
+    });
+  }
+  if (sanitizeErr) {
+    console.error(sanitizeErr);
+    return { err: sanitizeErr };
+  }
+
+  // invoking contract methods
+  try {
+    const tx = await baseContract.transferNFTsPublic(
+      hederaTokenAddress,
+      senders,
+      receivers,
+      serialNumbers
+    );
+
+    const txReceipt = await tx.wait();
+
+    // retrieve responseCode from event
+    const { data } = txReceipt.logs.filter(
+      (event: any) => event.fragment.name === 'ResponseCode'
+    )[0];
+
+    // @notice: 22 represents the predefined response code from the Hedera system contracts, indicating a successful transaction.
+    return { result: Number(data) === 22, transactionHash: txReceipt.hash };
+  } catch (err: any) {
+    console.error(err);
+    return { err, transactionHash: err.receipt && err.receipt.hash };
+  }
+};
+
+/**
+ * @dev transfers single token (fungible vs non-fungible)
+ *
+ * @dev integrates TokenTransferContract.transferTokenPublic()
+ *
+ * @dev integrates TokenTransferContract.transferNFTPublic()
+ *
+ * @param baseContract: ethers.Contract
+ *
+ * @param API: "FUNGIBLE" | "NON_FUNGIBLE"
+ *
+ * @param hederaTokenAddress: string
+ *
+ * @param sender: string
+ *
+ * @param receiver: string
+ *
+ * @param quantity: number (amount/serialNumber)
+ *
+ * @return Promise<TokenTransferSmartContractResult>
+ */
+export const transferSingleToken = async (
+  baseContract: Contract,
+  API: 'FUNGIBLE' | 'NON_FUNGIBLE',
+  hederaTokenAddress: string,
+  sender: string,
+  receiver: string,
+  quantity: number
+): Promise<TokenTransferSmartContractResult> => {
+  // sanitize params
+  let sanitizeErr;
+  if (!isAddress(hederaTokenAddress)) {
+    sanitizeErr = 'Invalid token address';
+  } else if (!isAddress(sender)) {
+    sanitizeErr = 'Invalid sender address';
+  } else if (!isAddress(receiver)) {
+    sanitizeErr = 'Invalid receiver address';
+  } else if (quantity < 0) {
+    sanitizeErr = 'Invalid quantity';
+  }
+  if (sanitizeErr) {
+    console.error(sanitizeErr);
+    return { err: sanitizeErr };
+  }
+
+  // invoking contract methods
+  try {
+    let transactionResult;
+    if (API === 'FUNGIBLE') {
+      transactionResult = await baseContract.transferTokenPublic(
+        hederaTokenAddress,
+        sender,
+        receiver,
+        quantity
+      );
+    } else {
+      transactionResult = await baseContract.transferNFTPublic(
+        hederaTokenAddress,
+        sender,
+        receiver,
+        quantity
+      );
+    }
+
+    const txReceipt = await transactionResult.wait();
+
+    // retrieve responseCode from event
+    const { data } = txReceipt.logs.filter(
+      (event: any) => event.fragment.name === 'ResponseCode'
+    )[0];
+
+    // @notice: 22 represents the predefined response code from the Hedera system contracts, indicating a successful transaction.
+    return { result: Number(data) === 22, transactionHash: txReceipt.hash };
+  } catch (err: any) {
+    console.error(err);
+    return { err, transactionHash: err.receipt && err.receipt.hash };
+  }
+};
+
+/**
+ * @dev transfers single token from token owner (fungible vs non-fungible)
+ *
+ * @dev integrates TokenTransferContract.transferFromPublic()
+ *
+ * @dev integrates TokenTransferContract.transferFromNFTPublic()
+ *
+ * @param baseContract: ethers.Contract
+ *
+ * @param API: "FUNGIBLE" | "NON_FUNGIBLE"
+ *
+ * @param hederaTokenAddress: string
+ *
+ * @param sender: string
+ *
+ * @param receiver: string
+ *
+ * @param quantity: number (amount/serialNumber)
+ *
+ * @return Promise<TokenTransferSmartContractResult>
+ */
+export const transferSingleTokenFrom = async (
+  baseContract: Contract,
+  API: 'FUNGIBLE' | 'NON_FUNGIBLE',
+  hederaTokenAddress: string,
+  sender: string,
+  receiver: string,
+  quantity: number
+): Promise<TokenTransferSmartContractResult> => {
+  // sanitize params
+  let sanitizeErr;
+  if (!isAddress(hederaTokenAddress)) {
+    sanitizeErr = 'Invalid token address';
+  } else if (!isAddress(sender)) {
+    sanitizeErr = 'Invalid sender address';
+  } else if (!isAddress(receiver)) {
+    sanitizeErr = 'Invalid receiver address';
+  } else if (quantity < 0) {
+    sanitizeErr = 'Invalid quantity';
+  }
+  if (sanitizeErr) {
+    console.error(sanitizeErr);
+    return { err: sanitizeErr };
+  }
+
+  // invoking contract methods
+  try {
+    let transactionResult;
+    if (API === 'FUNGIBLE') {
+      transactionResult = await baseContract.transferFromPublic(
+        hederaTokenAddress,
+        sender,
+        receiver,
+        quantity
+      );
+    } else {
+      transactionResult = await baseContract.transferFromNFTPublic(
+        hederaTokenAddress,
+        sender,
+        receiver,
+        quantity
+      );
+    }
+
+    const txReceipt = await transactionResult.wait();
+
+    // retrieve responseCode from event
+    const { data } = txReceipt.logs.filter(
+      (event: any) => event.fragment.name === 'ResponseCode'
+    )[0];
+
+    // @notice: 22 represents the predefined response code from the Hedera system contracts, indicating a successful transaction.
+    return { result: Number(data) === 22, transactionHash: txReceipt.hash };
+  } catch (err: any) {
+    console.error(err);
+    return { err, transactionHash: err.receipt && err.receipt.hash };
+  }
+};
