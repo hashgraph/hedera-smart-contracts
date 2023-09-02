9fbee51edc7f10198871912bf912b504672036f3
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-09-02 14:08:03 -0500

    dapp-feat: finished integrating ManageTokenDeduction APIs to interact with TokenManage contract (#369)
    * dapp-feat: finished integrating ManageTokenDeduction APIs to interact with TokenManage contract
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    * dapp-update: splited tokenDeduction UI tab into tokenSupplyReduction & tokenDelete
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    
    ---------
    
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- system-contract-dapp-playground/src/components/contract-interaction/hts/token-management-contract/methods/index.tsx
+++ system-contract-dapp-playground/src/components/contract-interaction/hts/token-management-contract/methods/index.tsx
@@ -21,8 +21,10 @@
 import { Contract } from 'ethers';
 import ManageTokenInfo from './manageTokenInfo';
 import ManageTokenStatus from './manageTokenStatus';
+import ManageTokenDelete from './manageTokenDelete';
 import ManageTokenRelation from './manageTokenRelation';
 import ManageTokenPermission from './manageTokenPermission';
+import ManageTokenDeduction from './manageTokenSupplyReduction';
 
 interface PageProps {
   method: string;
@@ -32,11 +34,12 @@ interface PageProps {
 const HederaTokenManagementMethods = ({ baseContract, method }: PageProps) => {
   return (
     <>
-      {method === 'tokenInformation' && <ManageTokenInfo baseContract={baseContract} />}
-      {method === 'tokenPermission' && <ManageTokenPermission baseContract={baseContract} />}
       {method === 'tokenStatus' && <ManageTokenStatus baseContract={baseContract} />}
+      {method === 'tokenDelete' && <ManageTokenDelete baseContract={baseContract} />}
+      {method === 'tokenInformation' && <ManageTokenInfo baseContract={baseContract} />}
       {method === 'tokenRelation' && <ManageTokenRelation baseContract={baseContract} />}
-      {method === 'tokenDeduction' && <>tokenDeduction</>}
+      {method === 'tokenPermission' && <ManageTokenPermission baseContract={baseContract} />}
+      {method === 'tokenSupplyReduction' && <ManageTokenDeduction baseContract={baseContract} />}
     </>
   );
 };
