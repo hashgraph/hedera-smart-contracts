4e5fd29714feb9b7ff200b5713d61dbe82190859
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-09-20 11:28:23 -0500

    dapp-feat: integrated ExchangeRate APIs to DApp Playground
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ system-contract-dapp-playground/__tests__/hedera/exchange-rate-interactions/index.test.ts
@@ -0,0 +1,81 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { Contract } from 'ethers';
+import { MOCK_GAS_LIMIT, MOCK_TX_HASH } from '../../utils/common/constants';
+import { handleExchangeRate } from '@/api/hedera/exchange-rate-interactions';
+
+describe('Exchange Rate Test Suite', () => {
+  const amount = 100000000;
+  const mockConvertedAmount = 833333;
+
+  // mock resolved return value
+  const contractMockedResolvedValue = (eventName: string) => {
+    return {
+      wait: jest.fn().mockResolvedValue({
+        logs: [
+          {
+            fragment: {
+              name: eventName,
+            },
+            data: mockConvertedAmount,
+          },
+        ],
+        hash: MOCK_TX_HASH,
+      }),
+    };
+  };
+
+  // mock baseContract object
+  const baseContract = {
+    convertTinycentsToTinybars: jest
+      .fn()
+      .mockResolvedValue(contractMockedResolvedValue('TinyBars')),
+    convertTinybarsToTinycents: jest
+      .fn()
+      .mockResolvedValue(contractMockedResolvedValue('TinyCents')),
+  };
+
+  it('should execute handleExchangeRate with API === "CENT_TO_BAR" and return a txHash and convertedAmount', async () => {
+    const txRes = await handleExchangeRate(
+      baseContract as unknown as Contract,
+      'CENT_TO_BAR',
+      amount,
+      MOCK_GAS_LIMIT
+    );
+
+    expect(txRes.err).toBeNull;
+    expect(txRes.transactionHash).toBe(MOCK_TX_HASH);
+    expect(txRes.convertedAmount).toBe(mockConvertedAmount);
+  });
+
+  it('should execute handleExchangeRate with API === "BAR_TO_CENT" and return a txHash and convertedAmount', async () => {
+    const txRes = await handleExchangeRate(
+      baseContract as unknown as Contract,
+      'BAR_TO_CENT',
+      amount,
+      MOCK_GAS_LIMIT
+    );
+
+    expect(txRes.err).toBeNull;
+    expect(txRes.transactionHash).toBe(MOCK_TX_HASH);
+    expect(txRes.convertedAmount).toBe(mockConvertedAmount);
+  });
+});
