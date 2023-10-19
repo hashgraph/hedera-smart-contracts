46ee8293d03492679a98771d601e9af6d6994d54
Stefan Stefanov <10372129+stefan-stefanooov@users.noreply.github.com>
2023-10-19 18:19:26 +0300

    Adding tests for scoping (#476)
    Adding tests for Scoping
    
    Signed-off-by: Stefan Stefanov <stefan.stefanooov@gmail.com>

---
+++ contracts/solidity/scoping/Scoping.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+contract Scoping {
+    function minimalScoping() pure public {
+        {
+            uint same;
+            same = 1;
+        }
+
+        {
+            uint same;
+            same = 3;
+        }
+    }
+
+    function reassign() pure public returns (uint) {
+        uint x = 1;
+        {
+            x = 2; // this will assign to the outer variable
+            uint x;
+        }
+        return x; // x has value 2
+    }
+}
