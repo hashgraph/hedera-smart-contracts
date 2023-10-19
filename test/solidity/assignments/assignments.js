daaec97732c409c7b05503160191c5ec0295d2b5
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2023-10-19 13:13:21 +0300

    Fixes some comments
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- test/solidity/assignments/assignments.js
+++ test/solidity/assignments/assignments.js
@@ -2,7 +2,7 @@ const { expect } = require('chai')
 const { ethers } = require('hardhat')
 const Constants = require('../../constants')
 
-describe('Test assignments', function () {
+describe('@solidityequiv1 Test assignments', function () {
   let signers;
 
   before(async function () {
@@ -21,6 +21,9 @@ describe('Test assignments', function () {
   })
 
   it('should verify assignment of reference types', async function () {
+    // here we are testing that if a parameter is assigned to memory a copy will be created
+    // and the original object wont be changed
+    // while if it is in storage and only referenced we expect it to change
     await contractReferenceTypes.testAssignmentOfReferenceTypes();
     const result = await contractReferenceTypes.getSomeArray();
     expect(result).to.deep.equal([ethers.BigNumber.from(1), ethers.BigNumber.from(2), ethers.BigNumber.from(3),
