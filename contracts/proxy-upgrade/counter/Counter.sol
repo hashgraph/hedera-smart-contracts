f4e4c92ea7c6e83852f2727839d6d900e977fb2b
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-10-27 12:45:59 -0500

    update: replaced OZ/SafeMath.sol to OZ/Math.sol
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/proxy-upgrade/counter/Counter.sol
+++ contracts/proxy-upgrade/counter/Counter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.5.0 <0.9.0;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
@@ -9,7 +9,7 @@ contract Counter is OwnableUpgradeable, UUPSUpgradeable {
     string public name;
     int256 public count;
 
-    using SafeMath for int256;
+    using Math for int256;
 
     function initialize(string memory _name) public initializer {
         name = _name;
