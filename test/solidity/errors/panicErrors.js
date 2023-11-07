5739d5c08a3cc3ab60d13b28bcf5bb091e74b54a
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-11-07 17:19:28 -0600

    update: renamed methods within Panic contract to fix foundry-test in CI (#578)
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- test/solidity/errors/panicErrors.js
+++ test/solidity/errors/panicErrors.js
@@ -31,93 +31,93 @@ describe('@solidityequiv3 Panic Errors', function () {
   })
 
   it('should verify panic error 0x01', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x01();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x01()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(1)])
   })
 
   it('should verify panic error 0x11', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x11();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x11()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(17)])
   })
 
   it('should verify panic error 0x12', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x12();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x12()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(18)])
   })
 
   it('should verify panic error 0x21', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x21();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x21()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(33)])
   })
 
   it('should verify panic error 0x31', async function () {
-    let error;
+    let error
     try {
-      const result = await contract.getSomeArray();
-      console.log(result);
-      await contract.testPanicError0x31();
-    } catch(e) {
-      error = e;
+      const result = await contract.getSomeArray()
+      console.log(result)
+      await contract.verifyPanicError0x31()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(18)])
   })
 
   it('should verify panic error 0x32', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x32();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x32()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(50)])
   })
 
   it('should verify panic error 0x41', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x41();
-    } catch(e) {
+      await contract.verifyPanicError0x41()
+    } catch (e) {
       console.log(e)
-      error = e;
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(65)])
   })
 
   it('should verify panic error 0x51', async function () {
-    let error;
+    let error
     try {
-      await contract.testPanicError0x51();
-    } catch(e) {
-      error = e;
+      await contract.verifyPanicError0x51()
+    } catch (e) {
+      error = e
     }
-    expect(error.errorName).to.eq('Panic');
+    expect(error.errorName).to.eq('Panic')
     expect(error.errorArgs).to.deep.eq([ethers.BigNumber.from(81)])
   })
 })
