c5cfa34f7d183c00b268762b0f50c219224aef8d
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-12-07 21:07:45 -0500

    fix: fixed ExampleGorverner contract
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/solidity/oz/governor/ExampleGovernor.sol
+++ contracts/solidity/oz/governor/ExampleGovernor.sol
@@ -1,88 +1,55 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import {IGovernor, Governor} from "@openzeppelin/contracts/governance/Governor.sol";
-import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
-import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import {GovernorTimelockControl} from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
-import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
-import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
-import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
-import"hardhat/console.sol";
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
-contract ExampleGovernor is
-    Governor,
-    GovernorCountingSimple,
-    GovernorVotes,
-    GovernorVotesQuorumFraction,
-    GovernorTimelockControl
-{
-    constructor(
-        IVotes _token,
-        TimelockController _timelock
-    ) Governor("ExampleGovernor") GovernorVotes(_token) GovernorVotesQuorumFraction(4) GovernorTimelockControl(_timelock) {}
+contract ExampleGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
+        Governor("ExampleGovernor")
+        GovernorSettings(0 /* 1 second */, 2 /* 2 blocks */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(1)
+    {}
 
-    function votingDelay() public pure override returns (uint256) {
-        return 0; // immediately
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
     }
 
-    function votingPeriod() public pure override returns (uint256) {
-        return 1; // 1 block
+    function votingPeriod()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
     }
 
-    function proposalThreshold() public pure override returns (uint256) {
-        return 0;
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(Governor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
     }
 
-    function _quorumReached(uint256 proposalId) internal view override(Governor, GovernorCountingSimple) returns (bool) {
-        return true; // return true for this example
-    }
-
-    // The functions below are overrides required by Solidity.
-
-    function state(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (ProposalState) {
-        return super.state(proposalId);
-    }
-
-    function proposalNeedsQueuing(
-        uint256 proposalId
-    ) public view virtual override(Governor, GovernorTimelockControl) returns (bool) {
-        return super.proposalNeedsQueuing(proposalId);
-    }
-
-    function _queueOperations(
-        uint256 proposalId,
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal override(Governor, GovernorTimelockControl) returns (uint48) {
-        console.log("ExampleGovernor._queueOperations");
-        return uint48(block.timestamp); // return current block timestamp for this example
-    }
-
-    function _executeOperations(
-        uint256 proposalId,
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal override(Governor, GovernorTimelockControl) {
-        console.log("ExampleGovernor._executeOperations");
-        super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
-    }
-
-    function _cancel(
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
-        return super._cancel(targets, values, calldatas, descriptionHash);
-    }
-
-    function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
-        return super._executor();
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
     }
 }
--- test/solidity/oz/governor/ExampleGovernor.js
+++ test/solidity/oz/governor/ExampleGovernor.js
@@ -1,11 +1,12 @@
 const { expect } = require('chai')
-const { ethers, waffle } = require('hardhat')
+const { ethers } = require('hardhat')
 
-describe('ExampleGovernor', function () {
-  let actions, exampleToken, governor, simpleStore, timelock
-  let deployer, user1, user2
-  let proposalId
-  const MIN_DELAY = 3600 // 1 hour in seconds
+describe.only('ExampleGovernor', function () {
+  let actions, exampleToken, governor, projectTeam, deployer, proposalId
+  const GAS_LIMIT = 1_000_000
+  const VOTE_WEIGHT = 1000
+  const VOTE_SUPPORT = 1 // 1 = For, 0 = Against, 2 = Abstain
+  const GRANT_AMOUNT = 100
 
   const description = 'Proposal #1: '
   const descriptionHash = ethers.utils.keccak256(
@@ -16,60 +17,34 @@ describe('ExampleGovernor', function () {
     ;[deployer, projectTeam] = await ethers.getSigners()
 
     // Deploy the token contract
-    const ExampleToken = await ethers.getContractFactory('ExampleToken') // Replace with your token contract
-    exampleToken = await ExampleToken.deploy()
+    const ExampleToken = await ethers.getContractFactory('ExampleToken')
+    exampleToken = await ExampleToken.deploy({ gasLimit: GAS_LIMIT })
     await exampleToken.deployed()
 
-    await exampleToken.mint(deployer.address, 1000)
-
-    // Deploy the TimelockController
-    const TimelockController = await ethers.getContractFactory(
-      'TimelockController'
-    )
-    timelock = await TimelockController.deploy(
-      MIN_DELAY,
-      [],
-      [],
-      deployer.address
-    )
-    await timelock.deployed()
+    await exampleToken.mint(deployer.address, VOTE_WEIGHT)
 
     // Deploy the ExampleGovernor
     const ExampleGovernor = await ethers.getContractFactory('ExampleGovernor')
-    governor = await ExampleGovernor.deploy(
-      exampleToken.address,
-      timelock.address
-    )
+    governor = await ExampleGovernor.deploy(exampleToken.address, {
+      gasLimit: GAS_LIMIT,
+    })
     await governor.deployed()
 
-    // const SimpleStore = await ethers.getContractFactory("SimpleStore");
-    // simpleStore = await SimpleStore.deploy();
-    // await simpleStore.deployed();
-
-    // const tokenAddress = ;
-    // const token = await ethers.getContractAt(‘ERC20’, tokenAddress);
-
-    const teamAddress = projectTeam.address
-    const grantAmount = 100
+    const teamAddress = await projectTeam.getAddress()
     const transferCalldata = exampleToken.interface.encodeFunctionData(
       'transfer',
-      [teamAddress, grantAmount]
+      [teamAddress, GRANT_AMOUNT]
     )
 
-    // const newStoreValue = 42;
-    // const encodedAction = simpleStore.interface.encodeFunctionData("setValue", [newStoreValue]);
-
     actions = {
-      targets: [deployer.address],
+      targets: [exampleToken.address],
       values: [0], // ETH value for the transaction, typically 0 for non-payable functions
       calldatas: [transferCalldata],
     }
   })
 
-  it('should allow creating proposals', async function () {
+  it('Should allow creating proposals', async function () {
     await exampleToken.connect(deployer).delegate(deployer.address) // Delegate votes
-    await ethers.provider.send('evm_mine', []) // Mine a block
-
     const tx = await governor
       .connect(deployer)
       .propose(actions.targets, actions.values, actions.calldatas, description)
@@ -78,63 +53,57 @@ describe('ExampleGovernor', function () {
     const event = receipt.events.find(
       (event) => event.event === 'ProposalCreated'
     )
+
     proposalId = event.args.proposalId
-    console.log(`DEBUG: In test, proposalId: ${proposalId}`)
-    const voteStart = event.args.voteStart
-    console.log(`DEBUG: voteStart: ${voteStart}`)
-    // const proposal = await governor.connect(deployer).getProposal(proposalId);
-    // console.log(`Debug: proposal: ${JSON.stringify(proposal)}`);
 
     expect(proposalId).to.not.be.undefined
     expect(event.args.description).to.eq(description)
+    expect(event.args.targets[0]).to.eq(exampleToken.address)
+    expect(event.args.proposer).to.eq(await deployer.getAddress())
   })
 
-  it('should allow voting on a proposal', async function () {
-    // const proposal = await governor.connect(deployer).getProposal(proposalId);
-    // console.log(`Debug: proposal: ${JSON.stringify(proposal)}`);
-    // await exampleToken.connect(deployer).delegate(deployer.address);
-    // const votingUnits = exampleToken.connect(deployer).getVotingUnits();
-    console.log('About to cast vote')
-    await governor.connect(deployer).castVote(proposalId, 1) // 1 = For, 0 = Against, 2 = Abstain
-    await ethers.provider.send('evm_mine', []) // Mine a block to process the vote
+  it('Should allow voting on a proposal', async function () {
+    const tx = await governor
+      .connect(deployer)
+      .castVote(proposalId, VOTE_SUPPORT)
+    const receipt = await tx.wait()
+    const event = receipt.events.find((e) => e.event === 'VoteCast')
+    const proposalDeadline = await governor.proposalDeadline(proposalId)
 
-    // Proposal Succeeded
+    const proposalState = await governor.state(proposalId)
+
+    expect(proposalState).to.eq(1) // active
+    expect(event.args.support).to.eq(VOTE_SUPPORT)
+    expect(event.args.proposalId).to.eq(proposalId)
+    expect(event.blockNumber).to.lte(proposalDeadline)
+    expect(event.args.voter).to.eq(await deployer.getAddress())
+    expect(event.args.weight).to.eq(ethers.BigNumber.from(VOTE_WEIGHT))
+  })
+
+  it('Should wait until the Vote Period passes and return a successful state on the proposal', async () => {
+    // wait for voting period to be over => Proposal Succeeded as the quorum has reached and the majority of the vote is in favor
+    let blockNum = await ethers.provider.getBlockNumber()
+    const proposalDeadline = await governor.proposalDeadline(proposalId)
+
+    while (blockNum <= proposalDeadline) {
+      new Promise((r) => setTimeout(r, 500))
+      blockNum = await ethers.provider.getBlockNumber()
+    }
+
     const proposalState = await governor.state(proposalId)
     expect(proposalState).to.equal(4) // 4 = Succeeded
   })
 
-  it('should execute a proposal after it is successful', async function () {
-    // Fast-forward time to surpass the voting period
-    await ethers.provider.send('evm_increaseTime', [604800]) // 7 days in seconds
-    await ethers.provider.send('evm_mine', [])
-
-    // Queue and execute the proposal
-    console.log(`DEBUG: desciptionHash: ${descriptionHash}`)
-
-    const encoded = ethers.utils.defaultAbiCoder.encode(
-      ['address[]', 'uint256[]', 'bytes[]', 'bytes32'],
-      [actions.targets, actions.values, actions.calldatas, descriptionHash]
-    )
-
-    console.log('Encoded Data:', encoded)
-
-    await governor
+  it('Should execute a proposal', async () => {
+    // in production, there should be a treasury account for this task
+    // funding governor contract
+    await exampleToken
       .connect(deployer)
-      .queue(
-        actions.targets,
-        actions.values,
-        actions.calldatas,
-        descriptionHash
-      )
-    await ethers.provider.send('evm_increaseTime', [MIN_DELAY + 1])
-    await ethers.provider.send('evm_mine', [])
-    console.log('DEBUG After queue')
+      .transfer(governor.address, GRANT_AMOUNT)
 
-    // await governor.connect(deployer).execute(proposalId);
-    console.log('About to execute')
-    console.log(
-      `Values: targets: ${actions.targets} value: ${actions.values} calldatas: ${actions.calldatas} descriptionHash: ${descriptionHash}`
-    )
+    expect(await exampleToken.balanceOf(governor.address)).to.eq(GRANT_AMOUNT)
+
+    // execute proposal
     await governor
       .connect(deployer)
       .execute(
@@ -143,11 +112,18 @@ describe('ExampleGovernor', function () {
         actions.calldatas,
         descriptionHash
       )
-    console.log('after execute')
 
-    // Check the proposal state
+    expect(await exampleToken.balanceOf(deployer.address)).to.eq(
+      ethers.BigNumber.from(VOTE_WEIGHT - GRANT_AMOUNT)
+    )
+    expect(await exampleToken.balanceOf(governor.address)).to.eq(
+      ethers.BigNumber.from(0)
+    )
+    expect(await exampleToken.balanceOf(projectTeam.address)).to.eq(
+      ethers.BigNumber.from(GRANT_AMOUNT)
+    )
+
     const proposalState = await governor.state(proposalId)
-    console.log('proposalState: %s', proposalState)
-    expect(proposalState).to.equal(7) // 7 = Executed
+    expect(proposalState).to.eq(7) // 7 = Executed
   })
 })
