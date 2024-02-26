d87c1f4e9dff2c4b266d8978a9010fa14f8220af
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-02-26 16:20:41 -0500

    update: updated sol@0.8.23 for util-precompile
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/util-precompile/IPrngSystemContract.sol
+++ contracts/util-precompile/IPrngSystemContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.4.9 <0.9.0;
+pragma solidity ^0.8.23;
 
 interface IPrngSystemContract {
     // Generates a 256-bit pseudorandom seed using the first 256-bits of running hash of n-3 transaction record.
