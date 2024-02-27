df005f8c97b83aa7de46edc3a5fa2342cd457f48
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-02-27 10:31:45 -0500

    update: updated sol@0.8.24 for proxy-upgrade
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/proxy-upgrade/exchange/Exchange.sol
+++ contracts/proxy-upgrade/exchange/Exchange.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
