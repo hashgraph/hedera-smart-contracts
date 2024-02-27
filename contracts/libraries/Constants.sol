a489862a9b730d5d9d86315039eb885e9d1136ec
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-02-27 10:02:35 -0500

    update: updated sol@0.8.24 for Multicall
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/libraries/Constants.sol
+++ contracts/libraries/Constants.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.24;
 
 abstract contract Constants {
   address internal constant HTS_PRECOMPILE = address(0x167);
