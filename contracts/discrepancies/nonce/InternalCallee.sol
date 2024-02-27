bf58066a647718aa5eb64c05f0da2d0ccedf7c29
Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>
2024-02-27 15:02:15 +0200

    test: first 4 tests
    Signed-off-by: Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>

--- contracts/discrepancies/nonce/InternalCallee.sol
+++ contracts/discrepancies/nonce/InternalCallee.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-contract Sample {}
+contract Sample {
+    function selfdestruct() external {
+        selfdestruct(payable(msg.sender));
+    }
+}
 
 contract InternalCallee {
     uint calledTimes = 0;
@@ -25,4 +29,19 @@ contract InternalCallee {
     function selfdestruct(address payable _addr) external {
         selfdestruct(_addr);
     }
+
+      function selfdestructSample(address payable sampleAddress) external {
+        Sample(sampleAddress).selfdestruct();
+    }
+
+    event DeployedContractAddress(address);
+
+     function deployViaCreate2(uint256 _salt) external returns (address) {
+        Sample temp = new Sample{salt: bytes32(_salt)}();
+        emit DeployedContractAddress(address(temp));
+        
+        return address(temp);
+    }
+
+
 }
