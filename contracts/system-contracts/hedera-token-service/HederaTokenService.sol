1d8789919ebcf06c0744406dfb0035435b7d6b10
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-11-13 12:02:32 -0600

    chore: added docs to HIP-904 related methods
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- contracts/system-contracts/hedera-token-service/HederaTokenService.sol
+++ contracts/system-contracts/hedera-token-service/HederaTokenService.sol
@@ -705,32 +705,42 @@ abstract contract HederaTokenService {
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
     }
 
-    function airdropTokens(IHederaTokenService.TokenTransferList[] memory tokenTransfers)
-    internal returns (int64 responseCode) {
+    /// @notice Airdrop one or more tokens to one or more accounts
+    /// @param tokenTransfers Array of token transfer lists containing token addresses and recipient details
+    /// @return responseCode The response code for the status of the request. SUCCESS is 22.
+    function airdropTokens(IHederaTokenService.TokenTransferList[] memory tokenTransfers) internal returns (int64 responseCode) {
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.airdropTokens.selector, tokenTransfers)
-        );
+            abi.encodeWithSelector(IHederaTokenService.airdropTokens.selector, tokenTransfers));
+            
         (responseCode) = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
     }
 
-    function cancelAirdrops(IHederaTokenService.PendingAirdrop[] memory pendingAirdrops)
-    internal returns (int64 responseCode) {
+    /// @notice Cancels pending airdrops that have not yet been claimed
+    /// @param pendingAirdrops Array of pending airdrops to cancel
+    /// @return responseCode The response code for the status of the request. SUCCESS is 22.
+    function cancelAirdrops(IHederaTokenService.PendingAirdrop[] memory pendingAirdrops) internal returns (int64 responseCode) {
         (bool success, bytes memory result) = precompileAddress.call(
             abi.encodeWithSelector(IHederaTokenService.cancelAirdrops.selector, pendingAirdrops)
         );
         (responseCode) = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
     }
 
-    function claimAirdrops(IHederaTokenService.PendingAirdrop[] memory pendingAirdrops)
-    internal returns (int64 responseCode) {
+    /// @notice Claims pending airdrops that were sent to the calling account
+    /// @param pendingAirdrops Array of pending airdrops to claim
+    /// @return responseCode The response code for the status of the request. SUCCESS is 22.
+    function claimAirdrops(IHederaTokenService.PendingAirdrop[] memory pendingAirdrops) internal returns (int64 responseCode) {
         (bool success, bytes memory result) = precompileAddress.call(
             abi.encodeWithSelector(IHederaTokenService.claimAirdrops.selector, pendingAirdrops)
         );
         (responseCode) = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
     }
 
-    function rejectTokens(address rejectingAddress, address[] memory ftAddresses, IHederaTokenService.NftID[] memory nftIds)
-    internal returns (int64 responseCode) {
+    /// @notice Rejects one or more tokens by transferring their full balance from the requesting account to the treasury
+    /// @param rejectingAddress The address rejecting the tokens
+    /// @param ftAddresses Array of fungible token addresses to reject
+    /// @param nftIds Array of NFT IDs to reject
+    /// @return responseCode The response code for the status of the request. SUCCESS is 22.
+    function rejectTokens(address rejectingAddress, address[] memory ftAddresses, IHederaTokenService.NftID[] memory nftIds) internal returns (int64 responseCode) {
         (bool success, bytes memory result) = precompileAddress.call(
             abi.encodeWithSelector(IHederaTokenService.rejectTokens.selector, rejectingAddress, ftAddresses, nftIds)
         );
--- contracts/system-contracts/hedera-token-service/IHRC904.sol
+++ contracts/system-contracts/hedera-token-service/IHRC904.sol
@@ -1,12 +1,48 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.4.9 <0.9.0;
 
+/// @dev See {https://github.com/hashgraph/hedera-services/blob/develop/hedera-node/docs/design/services/smart-contract-service/frictionless-airdrops-system-contracts.md#new-solidity-functions}
 interface IHRC904 {
+    /// @notice Cancels a pending fungible token airdrop to a specific receiver
+    /// @notice Responsible service: HTS
+    /// @param receiverAddress The address of the receiver whose airdrop should be cancelled
+    /// @return responseCode The response code indicating the result of the operation
     function cancelAirdropFT(address receiverAddress) external returns (int64 responseCode);
+
+    /// @notice Cancels a pending non-fungible token airdrop to a specific receiver
+    /// @notice Responsible service: HTS
+    /// @param receiverAddress The address of the receiver whose airdrop should be cancelled
+    /// @param serialNumber The serial number of the NFT to cancel
+    /// @return responseCode The response code indicating the result of the operation
     function cancelAirdropNFT(address receiverAddress, int64 serialNumber) external returns (int64 responseCode);
+
+    /// @notice Claims a pending fungible token airdrop from a specific sender
+    /// @notice Responsible service: HTS
+    /// @param senderAddress The address of the sender whose airdrop should be claimed
+    /// @return responseCode The response code indicating the result of the operation
     function claimAirdropFT(address senderAddress) external returns (int64 responseCode);
+
+    /// @notice Claims a pending non-fungible token airdrop from a specific sender
+    /// @notice Responsible service: HTS
+    /// @param senderAddress The address of the sender whose airdrop should be claimed
+    /// @param serialNumber The serial number of the NFT to claim
+    /// @return responseCode The response code indicating the result of the operation
     function claimAirdropNFT(address senderAddress, int64 serialNumber) external returns (int64 responseCode);
+
+    /// @notice Rejects all pending fungible token airdrops
+    /// @notice Responsible service: HTS
+    /// @return responseCode The response code indicating the result of the operation
     function rejectTokenFT() external returns (int64 responseCode);
+
+    /// @notice Rejects pending non-fungible token airdrops for specific serial numbers
+    /// @notice Responsible service: HTS
+    /// @param serialNumbers Array of NFT serial numbers to reject
+    /// @return responseCode The response code indicating the result of the operation
     function rejectTokenNFTs(int64[] memory serialNumbers) external returns (int64 responseCode);
+
+    /// @notice Enables or disables automatic token associations for the calling account
+    /// @notice Responsible service: HAS
+    /// @param enableAutoAssociations True to enable unlimited automatic associations, false to disable
+    /// @return responseCode The response code indicating the result of the operation
     function setUnlimitedAutomaticAssociations(bool enableAutoAssociations) external returns (int64 responseCode);
 }
