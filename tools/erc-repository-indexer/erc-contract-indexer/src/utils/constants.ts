8f61e914d4183d6c347a947604a4262c63b012e4
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-12-03 12:21:03 -0600

    feat: added utilitiy methods
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ tools/erc-repository-indexer/erc-contract-indexer/src/utils/constants.ts
@@ -0,0 +1,25 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+export default {
+  RETRY_DELAY_MS: 15000,
+  GET_CONTRACT_ENDPOINT: '/api/v1/contracts',
+  MIRROR_NODE_FALL_BACK_BASE_URL: 'https://previewnet.mirrornode.hedera.com',
+};
