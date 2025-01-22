f1c08a85be1457f5a1f47d1bb0299c446c65a1e3
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-01-21 19:05:38 -0600

    feat: added AliasAccountUtility() contract + test coverage
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ contracts/system-contracts/hedera-account-service/examples/IHRC-632/aliasAccountUtility.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "../../HederaAccountService.sol";
+
+
+contract AliasAccountUtility is HederaAccountService { 
+    event ResponseCode(int responseCode);
+    event IsAuthorizedRaw(address account, bool response);
+    event AddressAliasResponse(int64 responseCode, address evmAddressAlias);
+
+
+
+    /// Returns the EVM address alias for a given Hedera account.
+    /// @param accountNumAlias The Hedera account number alias to lookup. Must satisfy all of the following:
+    ///                         - Must be in long-zero format (0x000...0<account_num>)
+    ///                         - Must reference an existing Hedera account
+    ///                         - Referenced account must have an associated EVM address alias
+    /// @return responseCode The response code indicating the status of the request:
+    ///                         - SUCCESS (22) if successful
+    ///                         - INVALID_ACCOUNT_ID (15) if any validation of the accountNumAlias fails
+    /// @return evmAddressAlias The EVM address alias associated with the Hedera account, or address(0) if the request fails
+    function getEvmAddressAliasPublic(address accountNumAlias) public returns (int64 responseCode, address evmAddressAlias) 
+    {
+        (responseCode, evmAddressAlias) = HederaAccountService.getEvmAddressAlias(accountNumAlias);
+        emit AddressAliasResponse(responseCode, evmAddressAlias);
+        if (responseCode != HederaResponseCodes.SUCCESS && responseCode != HederaResponseCodes.INVALID_ACCOUNT_ID) {
+            revert();
+        }
+    }
+
+    /// Returns the Hedera Account ID (as account num alias) for the given EVM address alias
+    /// @param evmAddressAlias The EVM address alias to get the Hedera account for. Must satisfy all of the following:
+    ///                         - Must be in EVM format (not a long-zero address)
+    ///                         - Must reference an existing Hedera account
+    ///                         - Referenced account must have an associated account num alias (long-zero format)
+    /// @return responseCode The response code indicating the status of the request:
+    ///                         - SUCCESS (22) if successful
+    ///                         - INVALID_SOLIDITY_ADDRESS (29) if any validation of the evmAddressAlias fails
+    /// @return accountNumAlias The Hedera account's num alias in long-zero format, or address(0) if the request fails
+    function getHederaAccountNumAliasPublic(address evmAddressAlias) public returns (int64 responseCode, address accountNumAlias) {
+        (responseCode, accountNumAlias) = HederaAccountService.getHederaAccountNumAlias(evmAddressAlias);
+        emit AddressAliasResponse(responseCode, accountNumAlias);
+        if (responseCode != HederaResponseCodes.SUCCESS && responseCode != HederaResponseCodes.INVALID_SOLIDITY_ADDRESS) {
+            revert();
+        }
+    }
+
+    /// @notice Verifies if a signature was signed by the account's key(s)
+    /// @param account The account address to verify the signature against
+    /// @param messageHash The hash of the message that was signed
+    /// @param signature The signature to verify
+    /// @return responseCode The response code indicating success or failure
+    /// @return response True if the signature is valid for the account, false otherwise
+    function isAuthorizedRawPublic(address account, bytes memory messageHash, bytes memory signature) public returns (int64 responseCode, bool response) {
+        (responseCode, response) = HederaAccountService.isAuthorizedRaw(account, messageHash, signature);
+        emit ResponseCode(responseCode);
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+        emit IsAuthorizedRaw(account, response);
+    }
+}
