4ab8e0945a8d898ac5e16bdfda273e93db6b172c
nikolay <n.atanasow94@gmail.com>
2025-05-20 16:43:07 +0300

    chore: add tests
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ contracts/hip-1028/CreateTokenVersioned.sol
@@ -0,0 +1,261 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.5.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "./HederaTokenService/HederaResponseCodes.sol";
+import "./HederaTokenService/HederaTokenService.sol";
+import "./HederaTokenService/KeyHelper.sol";
+import "./HederaTokenService/FeeHelper.sol";
+
+contract CreateTokenVersioned is HederaTokenService, KeyHelper, FeeHelper {
+    event TokenAddress(address);
+    event TokenInfo(IHederaTokenService.TokenInfo);
+    event FungibleTokenInfo(IHederaTokenService.FungibleTokenInfo);
+    event NonFungibleTokenInfo(IHederaTokenService.NonFungibleTokenInfo);
+
+    function createTokenWithMetadata() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "testToken";
+        token.metadata = bytes("testmeta");
+        token.symbol = "test";
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](0);
+
+        (int responseCode, address tokenAddress) = HederaTokenService.createFungibleToken(token, 100, 4);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createTokenWithMetadataAndCustomFees() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "testToken";
+        token.metadata = bytes("testmeta");
+        token.symbol = "test";
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](0);
+        IHederaTokenService.FixedFee[] memory fixedFees = createSingleFixedFeeForHbars(10, address(this));
+        IHederaTokenService.FractionalFee[] memory fractionalFees = new IHederaTokenService.FractionalFee[](0);
+        (int responseCode, address tokenAddress) = HederaTokenService.createFungibleTokenWithCustomFees(token, 100, 8, fixedFees, fractionalFees);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createTokenWithMetadataAndKey() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "testToken";
+        token.metadata = bytes("testmeta");
+        token.symbol = "test";
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](1);
+        IHederaTokenService.TokenKey memory tokenKey = super.getSingleKey(KeyType.METADATA, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenKey;
+        (int responseCode, address tokenAddress) = HederaTokenService.createFungibleToken(token, 100, 4);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createTokenWithMetadataAndKeyAndCustomFees() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "testToken";
+        token.metadata = bytes("testmeta");
+        token.symbol = "test";
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](1);
+        IHederaTokenService.TokenKey memory tokenKey = super.getSingleKey(KeyType.METADATA, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenKey;
+        IHederaTokenService.FixedFee[] memory fixedFees = createSingleFixedFeeForHbars(10, address(this));
+        IHederaTokenService.FractionalFee[] memory fractionalFees = new IHederaTokenService.FractionalFee[](0);
+        (int responseCode, address tokenAddress) = HederaTokenService.createFungibleTokenWithCustomFees(token, 100, 8, fixedFees, fractionalFees);
+
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createNftWithMetadata() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "nft";
+        token.symbol = "nft";
+        token.metadata = bytes("testmeta");
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](1);
+        IHederaTokenService.TokenKey memory tokenSupplyKey = super.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenSupplyKey;
+        (int responseCode, address tokenAddress) = HederaTokenService.createNonFungibleToken(token);
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createNftWithMetadataAndCustomFees() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "nft";
+        token.symbol = "nft";
+        token.metadata = bytes("testmeta");
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](1);
+        IHederaTokenService.TokenKey memory tokenSupplyKey = super.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenSupplyKey;
+        IHederaTokenService.FixedFee[] memory fixedFees = createSingleFixedFeeForHbars(10, address(this));
+        IHederaTokenService.RoyaltyFee[] memory royaltyFees = new IHederaTokenService.RoyaltyFee[](0);
+        (int responseCode, address tokenAddress) = HederaTokenService.createNonFungibleTokenWithCustomFees(token, fixedFees, royaltyFees);
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createNftWithMetaAndKey() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "nft";
+        token.symbol = "nft";
+        token.metadata = bytes("testmeta");
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](2);
+        IHederaTokenService.TokenKey memory tokenKey = super.getSingleKey(KeyType.METADATA, KeyValueType.CONTRACT_ID, address(this));
+        IHederaTokenService.TokenKey memory tokenSupplyKey = super.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenKey;
+        token.tokenKeys[1] = tokenSupplyKey;
+        (int responseCode, address tokenAddress) = HederaTokenService.createNonFungibleToken(token);
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function createNftWithMetaAndKeyAndCustomFees() public payable returns (address createdAddress) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "nft";
+        token.symbol = "nft";
+        token.metadata = bytes("testmeta");
+        token.treasury = address(this);
+        token.tokenKeys = new IHederaTokenService.TokenKey[](2);
+        IHederaTokenService.TokenKey memory tokenMetaKey = super.getSingleKey(KeyType.METADATA, KeyValueType.CONTRACT_ID, address(this));
+        IHederaTokenService.TokenKey memory tokenSupplyKey = super.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys[0] = tokenMetaKey;
+        token.tokenKeys[1] = tokenSupplyKey;
+        IHederaTokenService.FixedFee[] memory fixedFees = createSingleFixedFeeForHbars(10, address(this));
+        IHederaTokenService.RoyaltyFee[] memory royaltyFees = new IHederaTokenService.RoyaltyFee[](0);
+        (int responseCode, address tokenAddress) = HederaTokenService.createNonFungibleTokenWithCustomFees(token, fixedFees, royaltyFees);
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+
+        createdAddress = tokenAddress;
+
+        HederaTokenService.mintToken(createdAddress, 0, new bytes[](1));
+
+        emit TokenAddress(createdAddress);
+    }
+
+    function getInformationForToken(address token) external returns (IHederaTokenService.TokenInfo memory tokenInfo) {
+        (int responseCode, IHederaTokenService.TokenInfo memory retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert ();
+        }
+
+        tokenInfo = retrievedTokenInfo;
+
+        emit TokenInfo(retrievedTokenInfo);
+    }
+
+    function getInformationForFungibleToken(address token) external returns (IHederaTokenService.FungibleTokenInfo memory fungibleTokenInfo) {
+        (int responseCode, IHederaTokenService.FungibleTokenInfo memory retrievedTokenInfo) = HederaTokenService.getFungibleTokenInfo(token);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert ();
+        }
+
+        fungibleTokenInfo = retrievedTokenInfo;
+
+        emit FungibleTokenInfo(retrievedTokenInfo);
+    }
+
+    function getInformationForNonFungibleToken(address token, int64 serialNumber) external returns (IHederaTokenService.NonFungibleTokenInfo memory nonFungibleTokenInfo) {
+        (int responseCode, IHederaTokenService.NonFungibleTokenInfo memory retrievedTokenInfo) = HederaTokenService.getNonFungibleTokenInfo(token, serialNumber);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert ();
+        }
+
+        nonFungibleTokenInfo = retrievedTokenInfo;
+
+        emit NonFungibleTokenInfo(retrievedTokenInfo);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function updateTokenMetadata(address token, string memory metadata) public {
+        IHederaTokenService.HederaToken memory tokenInfo;
+        tokenInfo.metadata = bytes(metadata);
+
+        (int256 responseCode) = HederaTokenService.updateTokenInfo(token, tokenInfo);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert();
+        }
+    }
+
+    function updateTokenKeys(address token, bytes memory ed25519, address contractID) public {
+        IHederaTokenService.TokenKey[] memory keys = new IHederaTokenService.TokenKey[](1);
+        keys[0] = getSingleKey(KeyType.METADATA, KeyValueType.CONTRACT_ID, contractID);
+        //metadata 7
+
+        (int256 responseCode) = HederaTokenService.updateTokenKeys(token, keys);
+        require(responseCode == HederaResponseCodes.SUCCESS);
+    }
+}
---
+++ test/hip-1028/HIP1028.js
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: Apache-2.0
+
+const {expect} = require('chai');
+const {ethers} = require('hardhat');
+const utils = require('../system-contracts/hedera-token-service/utils');
+
+describe.only('HIP1028 Test Suite', function () {
+  let signers;
+  let createTokenContract;
+  let tokenWithMetadataAndMetadataKeyAndCustomFeeAddress;
+  let nftWithMetadataAndMetadataKeyAndCustomFeeAddress;
+
+  const TWENTY_HBARS = '20000000000000000000';
+  const META_TEXT = 'testmeta';
+  const FIXED_FEE_AMOUNT = 10;
+
+  before(async () => {
+    signers = await ethers.getSigners();
+    createTokenContract = await ethers.getContractFactory('CreateTokenVersioned');
+    createTokenContract = await createTokenContract.deploy({gasLimit: 5_000_000});
+    await createTokenContract.waitForDeployment();
+  });
+
+  describe('create token', async () => {
+    it('should verify createTokenWithMetadata creates a fungible token with metadata', async () => {
+      const receipt = await (await createTokenContract.createTokenWithMetadata({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.be.null;
+      expect(info.custom_fees.fixed_fees).to.be.empty;
+      expect(info.custom_fees.fractional_fees).to.be.empty;
+    });
+
+    it('should verify createTokenWithMetadataAndCustomFees creates a fungible token with metadata and custom fee', async () => {
+      const receipt = await (await createTokenContract.createTokenWithMetadataAndCustomFees({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.be.null;
+      expect(info.custom_fees.fixed_fees).to.not.be.empty;
+      expect(info.custom_fees.fixed_fees[0].amount).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify createTokenWithMetadataAndKey creates a fungible token with metadata and metadata key', async () => {
+      const receipt = await (await createTokenContract.createTokenWithMetadataAndKey({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.not.be.null;
+      expect(info.custom_fees.fixed_fees).to.be.empty;
+      expect(info.custom_fees.fractional_fees).to.be.empty;
+    });
+
+    it('should verify createTokenWithMetadataAndKeyAndCustomFees creates a fungible token with metadata and metadata key and custom fee', async () => {
+      const receipt = await (await createTokenContract.createTokenWithMetadataAndKeyAndCustomFees({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      tokenWithMetadataAndMetadataKeyAndCustomFeeAddress = receipt.logs[0].args[0];
+      const info = await utils.asyncGetTokenInfoByMN(tokenWithMetadataAndMetadataKeyAndCustomFeeAddress);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.not.be.null;
+      expect(info.custom_fees.fixed_fees).to.not.be.empty;
+      expect(info.custom_fees.fixed_fees[0].amount).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify createNftWithMetadata creates a non-fungible token with metadata', async () => {
+      const receipt = await (await createTokenContract.createNftWithMetadata({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.be.null;
+      expect(info.custom_fees.fixed_fees).to.be.empty;
+      expect(info.custom_fees.royalty_fees).to.be.empty;
+    });
+
+    it('should verify createNftWithMetadataAndCustomFees creates a non-fungible token with metadata and custom fee', async () => {
+      const receipt = await (await createTokenContract.createNftWithMetadataAndCustomFees({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.be.null;
+      expect(info.custom_fees.fixed_fees).to.not.be.empty;
+      expect(info.custom_fees.fixed_fees[0].amount).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify createNftWithMetaAndKey creates a non-fungible token with metadata and metadata key', async () => {
+      const receipt = await (await createTokenContract.createNftWithMetaAndKey({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      const info = await utils.asyncGetTokenInfoByMN(receipt.logs[0].args[0]);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.not.be.null;
+      expect(info.custom_fees.fixed_fees).to.be.empty;
+      expect(info.custom_fees.royalty_fees).to.be.empty;
+    });
+
+    it('should verify createNftWithMetaAndKeyAndCustomFees creates a fungible token with metadata and metadata key and custom fee', async () => {
+      const receipt = await (await createTokenContract.createNftWithMetaAndKeyAndCustomFees({
+        value: TWENTY_HBARS,
+        gasLimit: 5_000_000
+      })).wait();
+
+      nftWithMetadataAndMetadataKeyAndCustomFeeAddress = receipt.logs[0].args[0];
+      const info = await utils.asyncGetTokenInfoByMN(nftWithMetadataAndMetadataKeyAndCustomFeeAddress);
+      expect(ethers.toUtf8String((ethers.decodeBase64(info.metadata)))).to.equal(META_TEXT);
+      expect(info.metadata_key).to.not.be.null;
+      expect(info.custom_fees.fixed_fees).to.not.be.empty;
+      expect(info.custom_fees.fixed_fees[0].amount).to.equal(FIXED_FEE_AMOUNT);
+    });
+  });
+
+  describe('get token info', async () => {
+    it('should verify getInformationForToken returns the correct metadata and metadata key and custom fee for a token', async () => {
+      const infoReceipt = await (
+          await createTokenContract.getInformationForToken(tokenWithMetadataAndMetadataKeyAndCustomFeeAddress, {gasLimit: 5_000_000})
+      ).wait();
+
+      const info = infoReceipt.logs[0].args[0];
+      // metadata
+      expect(ethers.toUtf8String(info[0][9])).to.equal(META_TEXT);
+      // metadata key
+      expect(info[0][7][7][0]).to.equal(128);
+      expect(info[0][7][7][1][2]).to.not.equal(ethers.ZeroAddress);
+      // custom fee
+      expect(info[5][0][0]).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify getInformationForToken returns the correct metadata and metadata key and custom fee for a token', async () => {
+      const infoReceipt = await (
+          await createTokenContract.getInformationForToken(nftWithMetadataAndMetadataKeyAndCustomFeeAddress, {gasLimit: 5_000_000})
+      ).wait();
+
+      const info = infoReceipt.logs[0].args[0];
+      // metadata
+      expect(ethers.toUtf8String(info[0][9])).to.equal(META_TEXT);
+      // metadata key
+      expect(info[0][7][7][0]).to.equal(128);
+      expect(info[0][7][7][1][2]).to.not.equal(ethers.ZeroAddress);
+      // custom fee
+      expect(info[5][0][0]).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify getInformationForFungibleToken returns the correct metadata and metadata key and custom fee for a token', async () => {
+      const infoReceipt = await (
+          await createTokenContract.getInformationForFungibleToken(tokenWithMetadataAndMetadataKeyAndCustomFeeAddress, {gasLimit: 5_000_000})
+      ).wait();
+
+      const info = infoReceipt.logs[0].args[0][0];
+      // metadata
+      expect(ethers.toUtf8String(info[0][9])).to.equal(META_TEXT);
+      // metadata key
+      expect(info[0][7][7][0]).to.equal(128);
+      expect(info[0][7][7][1][2]).to.not.equal(ethers.ZeroAddress);
+      // custom fee
+      expect(info[5][0][0]).to.equal(FIXED_FEE_AMOUNT);
+    });
+
+    it('should verify getInformationForNonFungibleToken returns the correct metadata and metadata key and custom fee for a token', async () => {
+      const infoReceipt = await (
+          await createTokenContract.getInformationForNonFungibleToken(nftWithMetadataAndMetadataKeyAndCustomFeeAddress, 1, {gasLimit: 5_000_000})
+      ).wait();
+
+      const info = infoReceipt.logs[0].args[0][0];
+      // metadata
+      expect(ethers.toUtf8String(info[0][9])).to.equal(META_TEXT);
+      // metadata key
+      expect(info[0][7][7][0]).to.equal(128);
+      expect(info[0][7][7][1][2]).to.not.equal(ethers.ZeroAddress);
+      // custom fee
+      expect(info[5][0][0]).to.equal(FIXED_FEE_AMOUNT);
+    });
+  });
+});
