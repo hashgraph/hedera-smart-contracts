51a76bb70a3d74375a59c9d0fc931105dcdcd941
nikolay <n.atanasow94@gmail.com>
2025-12-04 13:44:44 +0200

    chore: add interfaces
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ contracts/system-contracts/hedera-schedule-service/IHRCScheduleFacade.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.4.9 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "./IHRC755.sol";
+import "./IHRC1215.sol";
+
+interface IHRCScheduleFacade is IHRC755, IHRC1215 {
+}
